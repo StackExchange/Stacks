f15c9c732f6d65877ad38c8f946b7a1af979a510
Joshua Hynes <jhynes@stackoverflow.com>
2017-08-02 15:22:03 -0400

    MAJOR UPDATE: Rebuilt Site (#23)
    * New Chrome Update
    
    * Rebuilt Tokko.js, added in scroll spy, modularized navigation, added a second layout for when there isn't a secondary nav
    
    * Trying to fix the primary nav…but failing
    
    * Primary nav finally fixed. Thanks Jon Chan!
    
    * Updating icons page. All icons are now pulled in via a data file.
    
    * Removing my spacing helpers in favor of ones Aaron created. Updating HTML accordingly.
    
    * Add atomic color styles
    
    * Update spacing page to the new layout, splitting out items into data files
    
    * Update grid page
    
    * Trashing app.less — no longer needed. Everything moved over to stacks.less that's needed.
    
    * Minor Updates
    
     - Open the Primary Navigation by default for now. We can always close
    it back up once we have more items in Stacks
     - Adjust Stacks navigation styles since nav are open by default.
     - Fix some odd Safari bugs with position: sticky and CSS Grids
     - Add background color for inverse button
     - Remove logo bottom margin
     - Add in some spacing between description copy and code blocks
    
    * A few more updates to the nav…
    
    - Remove the link on section level pages for now
    - Remove margins within Stacks.less in favor of new margin atom classes

---
+++ assets/js/main.js
@@ -0,0 +1,86 @@
+var UIkit = {
+	init: function() {
+		this.setMinHeightForPreviews(); 		// this functions lets you set min-height for .stack-preview by setting 'data-minheight' attribute. useful for elements that are positioned absolutely.
+		this.visibleCodebase();					// it lets you select which patterns you gonna see by using <select> in sidebar.
+		this.filesStructure();					// simple function for styling that checks if element has children
+    this.toggleSubMenu();
+    this.scrollSpy();
+
+		var sections = $('.stacks-section > :header');
+		if ( sections.length ) {
+			$(".js-secondary-nav").buildMenu();
+		}
+
+		setTimeout(function() {
+			if (location.hash) {
+				var h = $(location.hash).offset().top;
+			    window.scrollTo(0, h);
+			}
+		}, 500); // lol this is bad
+	},
+
+
+	setMinHeightForPreviews: function() {
+		$('.stacks-section .-preview[data-minheight]').each(function() {
+			var minheight = $(this).data('minheight');
+			$(this).css('min-height', minheight);
+		});
+	},
+
+	visibleCodebase: function() {
+		$('select#uikit-codebase').on('change', function() {
+			var current = $(this).val(),
+				el = $('#uikit-content > section');
+			localStorage.setItem('whatCodebase', current);
+
+			switch (current) {
+			    case '0': // Both
+			        el.show();
+			        break;
+			    case '1': // Only Core
+			        el.hide().filter('[data-core]').show();
+			        break;
+			    case '2': // Core and common
+			        el.hide().filter(':not([data-careers])').show();
+			        break;
+			    case '3': // Careers
+			        el.hide().filter('[data-careers]').show();
+			        break;
+			    case '4': // Careers and common
+			        el.hide().filter(':not([data-core])').show();
+			        break;
+			}
+		});
+
+		if(localStorage.whatCodebase) {
+			$('select#uikit-codebase').val(localStorage.getItem('whatCodebase')).trigger('change');
+		}
+	},
+
+	filesStructure: function() {
+		var li = $('.tree-structure').find('li:has(ul)');
+		li.each(function() {
+			$(this).addClass('has-children');
+		});
+	},
+
+  toggleSubMenu: function() {
+    $(".js-subnav-toggle").on("click", function(e) {
+      e.preventDefault();
+
+      $(".js-subnav-menu").removeClass("ut-ds-none").addClass("ut-ds-none");
+      $(".js-subnav-section").removeClass("is-open");
+
+      $(this).parent().parent(".js-subnav-section").addClass("is-open");
+      $(this).parent().next(".js-subnav-menu").toggleClass("ut-ds-none");
+    });
+  },
+
+  scrollSpy: function() {
+    $("body").scrollspy({ target: "#stacks-subnav"})
+  }
+}
+
+$(function() {
+	UIkit.init();
+});
