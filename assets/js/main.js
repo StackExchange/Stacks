5ff3d07e13f1dabfc785520c254c1a8e492f17ee
Aaron Shekey <aaron@aaronshekey.com>
2017-10-20 13:18:49 -0500

    Consolidate a bit of JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,6 @@
-var UIkit = {
-  init: function() {
-    var sections = $('.stacks-section > :header');
-    if ( sections.length ) {
-      $(".js-secondary-nav").buildMenu();
-    }
-  },
-}
-
 $(function() {
-  UIkit.init();
+  var sections = $('.stacks-section > :header');
+  if ( sections.length ) {
+    $(".js-secondary-nav").buildMenu();
+  }
 });
