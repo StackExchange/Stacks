a1b4d50aac6d824e0789c796d7207b725342f582
Joshua Hynes <jhynes@stackoverflow.com>
2017-11-02 16:08:37 -0400

    Two updates: Adding in new type class names, updating text-size names to stated stops versus levels, cleaning up stack.less to not use utility classes because it creates a specifity nightmare with all the important tags, and adding in an autocomplete component for Ted.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,13 @@
 $(function() {
-  var sections = $('.stacks-section > :header');
-  if ( sections.length ) {
-    $(".js-secondary-nav").buildMenu();
-  }
+    var sections = $('.stacks-section > :header');
+    if ( sections.length ) {
+        $(".js-secondary-nav").buildMenu();
+    }
+
+    $(".js-autocomplete-trigger").on("focus", function () {
+        $(".js-autocomplete-list").addClass("is-visible");
+    });
+    $(".js-autocomplete-trigger").on("blur", function () {
+        $(".js-autocomplete-list").removeClass("is-visible");
+    });
 });
