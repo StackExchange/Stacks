6807b84bd59055f8854e89f1edba43c05d769680
Aaron Shekey <aaron@aaronshekey.com>
2018-04-05 17:46:43 -0500

    AJAXIFY the navigation
    You didn't know you needed this. But damn do you need this.

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -17,3 +17,66 @@ $(function() {
         e.stopPropagation();
     });
 });
+
+$.when($.ready).then(function() {
+    window.history.replaceState({
+        'href': window.location.href,
+        'title': $('head').filter('title').text(),
+        'nav': $(document).find('#nav').html(),
+        'content': $(document).find('#content').html()
+    }, '', window.location.href)
+
+    $('#nav').on('click', 'a', function (event) {
+
+        // Allow opening links in new tabs
+        if (event.metaKey) {
+          return
+        }
+
+        // Prevent following link
+        event.preventDefault()
+
+        // Get desired link
+        var href = $(this).attr('href')
+
+        // Make Ajax request to get the page content
+        $.ajax({
+          method: 'GET',
+          url: href,
+          cache: false,
+          dataType: 'html',
+        }).done(function(html) {
+
+            // Parse the HTML response
+            var title = $(html).filter('title').text()
+            var nav = $(html).find('#nav').html()
+            var content = $(html).find('#content').html()
+
+            // Update the page
+            $('head title').text(title)
+            $('#nav').html(nav)
+            $('#content').html(content)
+
+            // Scroll to the top of the page
+            $(document).scrollTop(0)
+
+            // Add page load to browser history
+            window.history.pushState({
+                'href': href,
+                'title': title,
+                'nav': $(html).find('#nav').html(),
+                'content': $(html).find('#content').html()
+            }, '', href)
+        })
+    })
+
+    // Load page history (for back/forward nav)
+    window.onpopstate = function(e) {
+        if(e.state){
+            // Update the page
+            $('title').text(e.state.title)
+            $('#nav').html(e.state.nav)
+            $('#content').html(e.state.content)
+        }
+    }
+})
