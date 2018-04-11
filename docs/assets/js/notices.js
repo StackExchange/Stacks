0fb485d6952e7cc4daa9f005198ef00f85930a92
Joshua Hynes <jhynes@stackoverflow.com>
2018-04-11 09:00:52 -0400

    Minor notice JS enhancements. Don't hardcode top values. Get the element heights and calculate where to play elements.

--- docs/assets/js/notices.js
+++ docs/assets/js/notices.js
@@ -1,29 +1,33 @@
 $(document).ready(function() {
+    var topnav = $(".js-stacks-topbar");
+    var topnavHeight = topnav.outerHeight();
+    var banner = $(".js-notice-banner");
+    var bannerHeight = banner.outerHeight();
+    var toast = $(".js-notice-toast");
+    var closeBtn = $(".js-notice-close");
 
     $(".js-notice-banner-open").click(function(e) {
-        $(".js-stack-topbar-example").css("top","").show();
-        $(".js-notice-banner").attr("aria-hidden","false").removeClass("s-notice__danger is-pinned");
+        topnav.css("top","").show();
+        banner.attr("aria-hidden","false").removeClass("s-notice__danger is-pinned");
     });
 
     $(".js-notice-banner-pinned-open").click(function(e) {
-        $(".js-stack-topbar-example").css("top","50px").show();
-        $(".js-notice-banner").attr("aria-hidden","false").addClass("s-notice__danger is-pinned");
+        topnav.css("top", bannerHeight + "px").show();
+        banner.attr("aria-hidden","false").addClass("s-notice__danger is-pinned");
     });
 
     $(".js-notice-toast-open").click(function(e) {
-        var banner = $(".js-notice-banner");
         var attr = banner.attr("aria-hidden");
-        var toast = $(".js-notice-toast");
+        var toastOffset = topnavHeight + bannerHeight + 16 + "px";
 
         if (typeof attr !== typeof undefined && attr == "false") {
-            toast.css("top","116px");
+            toast.css("top", toastOffset);
         }
         else {
             toast.css("top","");
         }
 
-        $(".js-notice-toast")
-            .queue(function() {
+        toast.queue(function() {
                 $(this).attr("aria-hidden","false").dequeue();
             })
             .delay(3000)
@@ -32,12 +36,12 @@ $(document).ready(function() {
             });
     });
 
-    $(".js-notice-close").click(function(e) {
+    closeBtn.click(function(e) {
         e.preventDefault();
         e.stopPropagation();
 
-        $(".js-notice-toast").attr("aria-hidden","true");
-        $(".js-stack-topbar-example").css("top","").hide();
-        $(".js-notice-banner").attr("aria-hidden","true").removeClass("s-notice__danger is-pinned");
+        toast.attr("aria-hidden","true");
+        topnav.css("top","").hide();
+        banner.attr("aria-hidden","true").removeClass("s-notice__danger is-pinned");
     });
 });
