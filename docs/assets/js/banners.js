8d1a6b5bd65c70efa53c1b886fe4bf7eb595e49c
Joshua Hynes <jhynes@stackoverflow.com>
2018-04-19 17:20:59 -0400

    Start working on the smaller footer banner.

--- docs/assets/js/banners.js
+++ docs/assets/js/banners.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
     var hero = $(".js-banner-hero");
-    var heroSmall = $(".js-banner-sm");
+    var heroSmall = $(".js-banner-bar");
     var heroBtn = $(".js-banner-show-hero");
     var heroTypeMenu = $(".js-hero-type-menu");
     var heroPatternMenu = $(".js-hero-pattern-menu");
     var heroPositionMenu = $(".js-hero-position-menu");
     var heroHeight = hero.innerHeight();
+    var heroBannerFooter = $(".js-banner-hero-footer");
 
     hero.hide();
     heroSmall.hide();
@@ -18,62 +19,71 @@ $(document).ready(function() {
         var circle = $(".js-banner-circle");
         var typeClasses = ("s-banner__bgg-info s-banner__bgg-important s-banner__bgg-danger s-banner__bgg-success s-banner__bgg-brand s-banner__bgg-pattern is-pinned");
 
-        // if (patternSelect == "true") {
-        //     alert("Patterns!");
-        // }
-        // else {
-        //     alert("It's false");
-        // }
-        // if (circleSelect = "true") {
-        //     alert("Circles!");
-        // }
-        // if (positionSelect == "is-pinned") {
-        //     hero
-        //         .show()
-        //         .attr("aria-hidden","false")
-        //         .removeClass(typeClasses)
-        //         .addClass(typeSelect + " " + positionSelect)
-        //         .css({
-        //             "position": "",
-        //             "top": "",
-        //             "left": "",
-        //             "right": ""
-        //         });
-        //     heroSmall
-        //         .hide()
-        //         .attr("aria-hidden","true")
-        //         .removeClass(typeClasses)
-        //         .css({
-        //             "position": "",
-        //             "top": "",
-        //             "left": "",
-        //             "right": ""
-        //         });
-        // }
-        // else {
-        //     hero
-        //         .show()
-        //         .attr("aria-hidden","false")
-        //         .removeClass(typeClasses)
-        //         .addClass(typeSelect)
-        //         .css({
-        //             "position": "fixed",
-        //             "top": "-4vw",
-        //             "left": "0",
-        //             "right": "0"
-        //         });
-        //     heroSmall
-        //         .show()
-        //         .attr("aria-hidden","false")
-        //         .removeClass(typeClasses)
-        //         .addClass(typeSelect).addClass(positionSelect)
-        //         .css({
-        //             "position": "fixed",
-        //             "top": heroHeight - 91,
-        //             "left": "0",
-        //             "right": "0"
-        //         });
-        // }
+        if (positionSelect == "is-pinned") {
+            heroBannerFooter
+                .show()
+                .attr("aria-hidden","false")
+                .addClass(typeSelect + " " + positionSelect);
+            hero
+                .hide()
+                .attr("aria-hidden","true")
+                .removeClass(typeClasses)
+                .css({
+                    "position": "",
+                    "top": "",
+                    "left": "",
+                    "right": ""
+                });
+            heroSmall
+                .hide()
+                .attr("aria-hidden","true")
+                .removeClass(typeClasses)
+                .css({
+                    "position": "",
+                    "top": "",
+                    "left": "",
+                    "right": ""
+                });
+            circle.removeClass("s-banner__bgg-circle");
+        }
+        else {
+            hero
+                .show()
+                .attr("aria-hidden","false")
+                .removeClass(typeClasses)
+                .addClass(typeSelect)
+                .css({
+                    "position": "fixed",
+                    "top": "-4vw",
+                    "left": "0",
+                    "right": "0"
+                });
+            heroSmall
+                .show()
+                .attr("aria-hidden","false")
+                .removeClass(typeClasses)
+                .addClass(typeSelect).addClass(positionSelect)
+                .css({
+                    "position": "fixed",
+                    "top": heroHeight - 94,
+                    "left": "0",
+                    "right": "0"
+                });
+
+            if (patternSelect == "yes") {
+                hero.addClass("s-banner__bgg-pattern");
+            }
+            else {
+                hero.removeClass("s-banner__bgg-pattern");
+            }
+
+            if (circleSelect == "yes") {
+                circle.addClass("s-banner__bgg-circle");
+            }
+            else {
+                circle.removeClass("s-banner__bgg-circle");
+            }
+        }
 
     });
 });
