8fde267f4d7c4dd7ed49b76cf3ae58f99da43671
Joshua Hynes <jhynes@stackoverflow.com>
2018-04-19 21:59:22 -0400

    Finish small footer banner.

--- docs/assets/js/banners.js
+++ docs/assets/js/banners.js
@@ -7,6 +7,8 @@ $(document).ready(function() {
     var heroPositionMenu = $(".js-hero-position-menu");
     var heroHeight = hero.innerHeight();
     var heroBannerFooter = $(".js-banner-hero-footer");
+    var circle = $(this).find(".js-banner-circle");
+    var typeClasses = ("s-banner__bgg-info s-banner__bgg-important s-banner__bgg-danger s-banner__bgg-success s-banner__bgg-brand s-banner__bgg-pattern is-pinned");
 
     hero.hide();
     heroSmall.hide();
@@ -16,13 +18,12 @@ $(document).ready(function() {
         var patternSelect = heroPatternMenu.find(":selected").data("pattern");
         var circleSelect = heroPatternMenu.find(":selected").data("circle");
         var positionSelect = heroPositionMenu.find(":selected").data("class");
-        var circle = $(".js-banner-circle");
-        var typeClasses = ("s-banner__bgg-info s-banner__bgg-important s-banner__bgg-danger s-banner__bgg-success s-banner__bgg-brand s-banner__bgg-pattern is-pinned");
 
         if (positionSelect == "is-pinned") {
             heroBannerFooter
                 .show()
                 .attr("aria-hidden","false")
+                .removeClass(typeClasses)
                 .addClass(typeSelect + " " + positionSelect);
             hero
                 .hide()
@@ -44,9 +45,25 @@ $(document).ready(function() {
                     "left": "",
                     "right": ""
                 });
-            circle.removeClass("s-banner__bgg-circle");
+            if (patternSelect == "yes") {
+                heroBannerFooter.addClass("s-banner__bgg-pattern");
+            }
+            else {
+                heroBannerFooter.removeClass("s-banner__bgg-pattern");
+            }
+
+            if (circleSelect == "yes") {
+                circle.addClass("s-banner__bgg-circle");
+            }
+            else {
+                circle.removeClass("s-banner__bgg-circle");
+            }
         }
         else {
+            heroBannerFooter
+                .hide()
+                .attr("aria-hidden","true")
+                .removeClass(typeClasses);
             hero
                 .show()
                 .attr("aria-hidden","false")
@@ -84,6 +101,36 @@ $(document).ready(function() {
                 circle.removeClass("s-banner__bgg-circle");
             }
         }
+    });
 
+    $(".js-banner-dismiss-btn").on("click", function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+
+        heroBannerFooter
+                .hide()
+                .attr("aria-hidden","true")
+                .removeClass(typeClasses);
+        heroSmall
+                .hide()
+                .attr("aria-hidden","true")
+                .removeClass(typeClasses)
+                .css({
+                    "position": "",
+                    "top": "",
+                    "left": "",
+                    "right": ""
+                });
+        hero
+            .hide()
+            .attr("aria-hidden","true")
+            .removeClass(typeClasses)
+            .css({
+                "position": "",
+                "top": "",
+                "left": "",
+                "right": ""
+            });
+        circle.removeClass("s-banner__bgg-circle");
     });
 });
