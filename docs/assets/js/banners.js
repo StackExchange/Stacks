f69be9a92774cf10ec9a2e6b53f6a0281e71c33a
Joshua Hynes <jhynes@stackoverflow.com>
2018-05-08 09:00:00 -0400

    Update the documentation. Change some of the class names.

--- docs/assets/js/banners.js
+++ docs/assets/js/banners.js
@@ -2,13 +2,14 @@ $(document).ready(function() {
     var hero = $(".js-banner-hero");
     var heroSmall = $(".js-banner-bar");
     var heroBtn = $(".js-banner-show-hero");
+    var heroCloseBtn = $(".js-banner-remove-hero");
     var heroTypeMenu = $(".js-hero-type-menu");
     var heroPatternMenu = $(".js-hero-pattern-menu");
     var heroPositionMenu = $(".js-hero-position-menu");
     var heroHeight = hero.outerHeight();
     var heroBannerFooter = $(".js-banner-hero-footer");
     var circle = $(this).find(".js-banner-circle");
-    var typeClasses = ("s-notice__info s-notice__brand s-notice__success s-notice__warning s-notice__danger s-notice__important s-notice--banner__info s-notice--banner__important s-notice--banner__danger s-notice--banner__success s-notice--banner__brand is-pinned");
+    var typeClasses = ("s-notice__info s-notice__brand s-notice__success s-notice__warning s-notice__danger s-notice__important s-notice__banner__info s-notice__banner__important s-notice__banner__danger s-notice__banner__success s-notice__banner__brand is-pinned");
 
     //  Hide everything on load
     hero.hide();
@@ -20,6 +21,9 @@ $(document).ready(function() {
         var typeSelect = heroTypeMenu.find(":selected").data("class");
         var circleSelect = heroPatternMenu.find(":selected").data("circle");
         var positionSelect = heroPositionMenu.find(":selected").data("class");
+
+        $(this).text("Update example");
+        heroCloseBtn.removeClass("d-none");
 
         if (positionSelect == "is-pinned") {
             heroBannerFooter
@@ -49,10 +53,10 @@ $(document).ready(function() {
                 });
 
             if (circleSelect == "yes") {
-                circle.addClass("s-notice--banner__circle");
+                circle.addClass("s-notice__banner-circle");
             }
             else {
-                circle.removeClass("s-notice--banner__circle");
+                circle.removeClass("s-notice__banner-circle");
             }
         }
         else {
@@ -84,12 +88,46 @@ $(document).ready(function() {
                 });
 
             if (circleSelect == "yes") {
-                circle.addClass("s-notice--banner__circle");
+                circle.addClass("s-notice__banner-circle");
             }
             else {
-                circle.removeClass("s-notice--banner__circle");
+                circle.removeClass("s-notice__banner-circle");
             }
         }
+    });
+
+    heroCloseBtn.on("click", function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+
+        heroBannerFooter
+                .hide()
+                .attr("aria-hidden","true")
+                .removeClass(typeClasses);
+        heroSmall
+                .hide()
+                .attr("aria-hidden","true")
+                .removeClass(typeClasses)
+                .css({
+                    "position": "",
+                    "top": "",
+                    "left": "",
+                    "right": ""
+                });
+        hero
+            .hide()
+            .attr("aria-hidden","true")
+            .removeClass(typeClasses)
+            .css({
+                "position": "",
+                "top": "",
+                "left": "",
+                "right": ""
+            });
+        circle.removeClass("s-notice__banner-circle");
+
+        heroBtn.text("Show example");
+        heroCloseBtn.addClass("d-none");
     });
 
     //  Dismiss the small hero footer banner
@@ -121,7 +159,7 @@ $(document).ready(function() {
                 "left": "",
                 "right": ""
             });
-        circle.removeClass("s-notice--banner__circle");
+        circle.removeClass("s-notice__banner-circle");
     });
 
 
