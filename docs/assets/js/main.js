17e3c55b2dd764610c2a3d1d2e6705696e8e7c5a
Joshua Hynes <jhynes@stackoverflow.com>
2018-06-22 16:50:28 -0400

    Split out search focus into its own file and include only that file in the homepage layout.

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -97,25 +97,4 @@ $(document).ready(function() {
             regenerateMenu();
         }
     });
-
-    //  Focus on the search bar
-    function focusSearch(e) {
-        if (e.which == 191) {
-            e.preventDefault();
-            $(".js-stacks-search-bar").focus();
-        }
-
-        $(".js-stacks-search-bar").keydown(function(e) {
-            e.stopPropagation();
-
-            if (e.which == 27) {
-                $(this).blur();
-            }
-        });
-    }
-
-    $(this).keydown(function(e) {
-        focusSearch(e);
-    });
-
 });
