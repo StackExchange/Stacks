4869637e76e3800155926ba52c341f3eb210a350
Joshua Hynes <jhynes@stackoverflow.com>
2018-07-02 22:14:42 -0400

    Split main.js up into two files. Load the files we need everywhere within head.html and only add the two menu js files we need on the page.html layout file.

--- docs/assets/js/search.js
+++ docs/assets/js/search.js
@@ -1,4 +1,19 @@
 $(document).ready(function() {
+    $(function() {
+        var search = docsearch({
+            apiKey: '09d35fa9def5c025244d5b217778a652',
+            indexName: 'stackoverflow_design',
+            inputSelector: '#searchbox',
+            debug: false
+        });
+
+        // Kill default behavior on empty links
+        $("a[href='#']").on("click", function(e) {
+            e.preventDefault();
+            e.stopPropagation();
+        });
+    });
+
     //  Focus on the search bar
     function focusSearch(e) {
         if (e.which == 191) {
