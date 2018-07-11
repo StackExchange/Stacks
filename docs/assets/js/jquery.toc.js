131c8d32468495fe4f21bee7ee1c88b612250c28
Aaron Shekey <aaron@aaronshekey.com>
2018-07-10 22:37:57 -0500

    Restore the sidebar menu generation

--- docs/assets/js/jquery.toc.js
+++ docs/assets/js/jquery.toc.js
@@ -80,7 +80,7 @@
                     // In the upside-down stack, unshift = push, and stack[0] = the top.
                     var parentItem = stack[0].children("li:last")[0];
                     if (parentItem) {
-                        stack.unshift($("<" + listTag + " class='grid fd-column stacks-nav__secondary'/>").appendTo(parentItem));
+                        stack.unshift($("<" + listTag + " class='grid fd-column list-reset stacks-nav__secondary'/>").appendTo(parentItem));
                     }
                 } else {
                     // Truncate the stack to the current level by chopping off the 'top' of the
@@ -90,8 +90,8 @@
                 }
 
                 // Add the list item
-                $("<li class='stacks-nav--item grid fd-column' />").appendTo(stack[0]).append(
-                    $("<a class='grid--cell stacks-nav--link fs-body1' />").text(elem.text()).attr("href", "#" + elem.attr("id"))
+                $("<li class='grid--cell fd-column stacks-nav--item' />").appendTo(stack[0]).append(
+                    $("<a class='d-block s-link s-link__muted fs-body1' />").text(elem.text()).attr("href", "#" + elem.attr("id"))
                 );
 
                 currentLevel = level;
