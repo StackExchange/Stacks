3cfc9d822300870501dd31699ed4ffca99aa5c57
Joshua Hynes <jhynes@stackoverflow.com>
2018-08-16 16:31:56 -0400

    Update JS for tooltips. It mostly works.

--- docs/assets/js/feature.popups.js
+++ docs/assets/js/feature.popups.js
@@ -12,6 +12,7 @@ $(document).ready(function() {
     //      This updates the tooltip or popover example
     //
     var menuUpdate = $(".js-menu-update");
+    var menuArrow = $(".js-menu-arrow");
     var menuPosition = $(".js-menu-position");
     var menuMethod = $(".js-menu-method");
 
@@ -63,24 +64,161 @@ $(document).ready(function() {
             else {
                 $(item).hideItem();
             }
+        },
+        mousemove: function(e) {
+            var target = $(this).attr("aria-controls");
+            var item = "#" + target;
+            var itemPos = $(this).position();
+            var pos = $(item).data("position");
+            var itemArrow = $(item).data("arrow");
+            var targetHeight = $(item).outerHeight();
+            var targetWidth = $(item).outerWidth();
+            var tipHeight = $(this).outerHeight();
+            var tipWidth = $(this).outerWidth();
+
+            if (pos === "bottom left") {
+                $(item).css({
+                    top: -(targetHeight + 8),
+                    left: itemPos.left - 8
+                });
+            }
+            else if (pos === "bottom center") {
+                $(item).css({
+                    top: -(targetHeight + 8),
+                    left: (tipWidth - targetWidth) / 2
+                });
+            }
+            else if (pos === "bottom right") {
+                $(item).css({
+                    top: -(targetHeight + 8),
+                    left: tipWidth - (targetWidth - 4)
+                });
+            }
+            else if (pos === "top left") {
+                $(item).css({
+                    top: tipHeight + 8,
+                    left: itemPos.left - 8
+                });
+            }
+            else if (pos === "top center") {
+                $(item).css({
+                    top: tipHeight + 8,
+                    left: (tipWidth - targetWidth) / 2
+                });
+            }
+            else if (pos === "top right") {
+                $(item).css({
+                    top: tipHeight + 8,
+                    left: tipWidth - targetWidth + 6
+                });
+            }
+            else if (pos === "left top") {
+                $(item).css({
+                    top: itemPos.top - 4,
+                    left: tipWidth + 8
+                });
+            }
+            else if (pos === "left center") {
+                $(item).css({
+                    top: -((targetHeight - tipHeight) / 2),
+                    left: tipWidth + 8
+                });
+            }
+            else if (pos === "left bottom") {
+                $(item).css({
+                    top: -(targetHeight - tipHeight),
+                    left: tipWidth + 8
+                });
+            }
+            else if (pos === "right top") {
+                $(item).css({
+                    top: itemPos.top - 4,
+                    left: -(targetWidth + 12)
+                });
+            }
+            else if (pos === "right center") {
+                $(item).css({
+                    top: -((targetHeight - tipHeight) / 2),
+                    left: -(targetWidth + 12)
+                });
+            }
+            else if (pos === "right bottom") {
+                $(item).css({
+                    top: -(targetHeight - tipHeight),
+                    left: -(targetWidth + 12)
+                });
+            }
         }
     });
 
 
+    //  --  Update the arrow options with the intitial direction is selected
+    menuPosition.change(function() {
+        var pos = $(this).find(":selected").data("position");
+        if ((pos === "top") || (pos ==="bottom")) {
+            var xOptions = {
+                "Left": "left",
+                "Center": "center",
+                "Right": "right"
+            };
+
+            menuArrow.empty();
+            $.each(xOptions, function(key,val) {
+                menuArrow.append($("<option></option>")
+                        .attr({
+                            "name": "tooltip-arrow",
+                            "id": val,
+                            "data-position": val
+                        })
+                        .text(key));
+                });
+            //menuArrow.find("option:eq(0)").attr("selected","");
+        }
+
+        if ((pos === "left") || (pos ==="right")) {
+            var yOptions = {
+                "Top": "top",
+                "Center": "center",
+                "Bottom": "bottom"
+            };
+
+            menuArrow.empty();
+            $.each(yOptions, function(key,val) {
+                menuArrow.append($("<option></option>")
+                        .attr({
+                            "name": "tooltip-arrow",
+                            "id": val,
+                            "data-position": val
+                        })
+                        .text(key));
+                });
+            //menuArrow.find("option:eq(0)").attr("selected","");
+        }
+    })
     //  --  Update the example on the page based on the menu options
     menuUpdate.on("click", function(e) {
         e.stopPropagation;
         e.preventDefault;
 
-        var position = menuPosition.find(":selected").data("class");
+        var pos1 = menuPosition.find(":selected").data("position");
+        var pos2 = menuArrow.find(":selected").data("position");
+        var pos = pos1 + " " + pos2;
+        var arr1 = pos1.charAt(0);
+        var arr2 = pos2.charAt(0);
         var method = menuMethod.find(":selected").data("method");
 
-        if (tooltip) {
-            tooltip
-                .removeClass(tipDirClass)
-                .addClass(position)
-                .data("method", method);
-        }
+        tooltip
+            .attr({
+                "data-method": method,
+                "data-position": pos
+            })
+            .css({
+                top: "",
+                left: ""
+            })
+            .find("[class^='s-arrow__']")
+            .removeClass(tipDirClass)
+            .addClass("s-arrow__" + arr1 + arr2);
     })
 
 
