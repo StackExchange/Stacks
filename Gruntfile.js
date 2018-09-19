ff4bac80c937ffcbd24b8ed5564fdddf4a851d45
Benjamin Dumke-von der Ehe <mail@benjamin-dumke.de>
2018-09-19 14:57:05 +0200

    port the table sorter from #134 to stimulus
    Not sure about the complete architecture yet, but it works.
    
    Also organize the JS a bit.

--- Gruntfile.js
+++ Gruntfile.js
@@ -56,8 +56,18 @@ module.exports = function(grunt) {
         uglify: {
             production: {
                 files: {
-                    'lib/js/dist/stacks.min.js': ['node_modules/stimulus/dist/stimulus.umd.js', 'node_modules/popper.js/dist/umd/popper.min.js', 'lib/js/src/stacks.js'],
-                    'docs/assets/js/stacks.min.js': ['node_modules/stimulus/dist/stimulus.umd.js', 'node_modules/popper.js/dist/umd/popper.min.js', 'lib/js/src/stacks.js']
+                    'lib/js/dist/stacks.min.js': [
+                        'node_modules/stimulus/dist/stimulus.umd.js',
+                        'node_modules/popper.js/dist/umd/popper.min.js',
+                        'lib/js/src/stacks.js',
+                        'lib/js/src/controllers/**/*.js'
+                    ],
+                    'docs/assets/js/stacks.min.js': [
+                        'node_modules/stimulus/dist/stimulus.umd.js',
+                        'node_modules/popper.js/dist/umd/popper.min.js',
+                        'lib/js/src/stacks.js',
+                        'lib/js/src/controllers/**/*.js'
+                    ],
                 }
             }
         },
