0757e115eeba274165e5fe07007ef96a4cfbface
Aaron Shekey <aaron@aaronshekey.com>
2018-09-23 00:29:24 -0500

    Initialize Backstop

---
+++ backstop_data/html_report/config.js
@@ -0,0 +1,40 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Stacks_Homepage_0_document_0_phone.png",
+        "test": "../bitmaps_test/20180923-002621/backstop_default_Stacks_Homepage_0_document_0_phone.png",
+        "selector": "document",
+        "fileName": "backstop_default_Stacks_Homepage_0_document_0_phone.png",
+        "label": "Stacks Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://127.0.0.1:4000/",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "error": "Reference file not found /Users/shekey/Repositories/StackOverflow/Stacks/backstop_data/bitmaps_reference/backstop_default_Stacks_Homepage_0_document_0_phone.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Stacks_Homepage_0_document_1_tablet.png",
+        "test": "../bitmaps_test/20180923-002621/backstop_default_Stacks_Homepage_0_document_1_tablet.png",
+        "selector": "document",
+        "fileName": "backstop_default_Stacks_Homepage_0_document_1_tablet.png",
+        "label": "Stacks Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://127.0.0.1:4000/",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "error": "Reference file not found /Users/shekey/Repositories/StackOverflow/Stacks/backstop_data/bitmaps_reference/backstop_default_Stacks_Homepage_0_document_1_tablet.png"
+      },
+      "status": "fail"
+    }
+  ],
+  "id": "backstop_default"
+});
