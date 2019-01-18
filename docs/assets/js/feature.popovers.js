277dd19ac2d9ee2f062facdb4ccc5bc66d658f54
Aaron Shekey <aaron@aaronshekey.com>
2019-01-18 14:28:17 -0600

    Address feedback

--- docs/assets/js/feature.popovers.js
+++ docs/assets/js/feature.popovers.js
@@ -3,22 +3,6 @@ $(document).ready(function() {
     var popoverExample = $(".js-popover-example");
 
     popoverBtn.click(function(e) {
-        popoverExample.toggleAttrVal("aria-hidden", "true", "false");
+        popoverExample.toggleClass("is-visible");
     });
 });
-
-// jquery toggle just the attribute value
-$.fn.toggleAttrVal = function(attr, val1, val2) {
-    var test = $(this).attr(attr);
-    if ( test === val1) {
-      $(this).attr(attr, val2);
-      return this;
-    }
-    if ( test === val2) {
-      $(this).attr(attr, val1);
-      return this;
-    }
-    // default to val1 if neither
-    $(this).attr(attr, val1);
-    return this;
-};
