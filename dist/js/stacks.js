0d9290abefbc9e40f10e41a042bb125de4a5ccb2
Benjamin Dumke-von der Ehe <mail@benjamin-dumke.de>
2019-01-23 13:16:18 +0100

    make `s-collapsible` a stand-alone thing instead of an appendix on `s-widget`, and provide a `s-collapsible-control` JavaScript component for the default behavior

--- dist/js/stacks.js
+++ dist/js/stacks.js
@@ -1,6 +1,6 @@
 /*
-Stimulus 1.1.0
-Copyright © 2018 Basecamp, LLC
+Stimulus 1.1.1
+Copyright © 2019 Basecamp, LLC
  */
 (function(global, factory) {
   typeof exports === "object" && typeof module !== "undefined" ? factory(exports) : typeof define === "function" && define.amd ? define([ "exports" ], factory) : factory(global.Stimulus = {});
@@ -1816,6 +1816,52 @@ Copyright © 2018 Basecamp, LLC
         Stacks.controllers[name] = Controller;
     };
 })()
+
+
+;
+
+(function () {
+    "use strict";
+
+    function listener(e) {
+        if (e.type == "keydown" && (e.keyCode != 13 && e.keyCode != 32)) {
+            return;
+        }
+        if (e.target !== e.currentTarget && ["A", "BUTTON"].indexOf(e.target.nodeName) >= 0) {
+            return;
+        }
+        var expanded = this.getAttribute("aria-expanded") === "true";
+        this.setAttribute("aria-expanded", expanded ? "false" : "true");
+        var l = document.getElementById(this.getAttribute("aria-controls"));
+        if (expanded) {
+            l.classList.add("is-collapsed");
+        } else {
+            l.classList.remove("is-collapsed");
+        }
+        if (e.type === "click") {
+            this.blur();
+        }
+        e.preventDefault();
+    }
+
+    Stacks.addController("s-collapsible-control", {
+
+        connect: function () {
+            this.element.addEventListener("click", listener);
+            this.element.addEventListener("keydown", listener);
+
+            var l = document.getElementById(this.element.getAttribute("aria-controls"));
+            var currentlyCollapsed = l.classList.contains("is-collapsed");
+            this.element.setAttribute("aria-expanded", currentlyCollapsed ? "false" : "true");
+        },
+
+        disconnect: function () {
+            this.element.removeEventListener("click", listener);
+            this.element.removeEventListener("keydown", listener);
+        }
+
+    });
+})();
 
 
 ;
