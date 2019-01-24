19ab9cb1e27da146e25f9134553f2ef665277071
Benjamin Dumke-von der Ehe <mail@benjamin-dumke.de>
2019-01-24 17:29:29 +0100

    `s-collapsible-control`: fix bugs; add comments; correct initial state on example page

--- dist/js/stacks.js
+++ dist/js/stacks.js
@@ -1829,6 +1829,15 @@ Copyright © 2019 Basecamp, LLC
 (function () {
     "use strict";
 
+    // Radio buttons only trigger a change event when they're *checked*, but not when
+    // they're *unchecked*. Therefore, if we have an active `s-collapsible-control` in
+    // the document, we listen for change events on *all* radio buttons and find any
+    // other radio buttons in the same `name` group, triggering a custom event on all
+    // of them so the controller can re-evaluate.
+    //
+    // We're keeping a count of how many of these controllers are connected to the DOM,
+    // so only have this global listener when we actually need it.
+
     var RADIO_OFF_EVENT = "s-collapsible-control:radio-off";
 
     function globalChangeListener(e) {
@@ -1871,10 +1880,11 @@ Copyright © 2019 Basecamp, LLC
     Stacks.addController("s-collapsible-control", {
 
         initialize: function () {
-            if (this.element.nodeName === "INPUT" && ["radio", "checkbox"].indexOf(this.type)) {
+            if (this.element.nodeName === "INPUT" && ["radio", "checkbox"].indexOf(this.element.type) >= 0) {
                 this.isCollapsed = this._isCollapsedForCheckable;
                 this.events = ["change", RADIO_OFF_EVENT];
                 this.isCheckable = true;
+                this.isRadio = this.element.type === "radio";
             } else {
                 this.isCollapsed = this._isCollapsedForClickable;
                 this.events = ["click", "keydown"];
@@ -1882,15 +1892,19 @@ Copyright © 2019 Basecamp, LLC
             this.listener = this.listener.bind(this);
         },
 
+        // for non-checkable elements, the initial source of truth is the collapsed/expanded
+        // state of the controlled element (unless the element doesn't exist)
         _isCollapsedForClickable: function () {
-            return this.controlledCollapsible().classList.contains("is-collapsed");
+            var cc = this.controlledCollapsible;
+            return cc ? cc.classList.contains("is-collapsed") : this.element.getAttribute("aria-expanded") === "false";
         },
 
+        // for checkable elements, the initial source of truth is the checked state
         _isCollapsedForCheckable: function () {
             return !this.element.checked;
         },
 
-        controlledCollapsible: function () {
+        get controlledCollapsible() {
             return document.getElementById(this.element.getAttribute("aria-controls"));
         },
 
@@ -1912,7 +1926,7 @@ Copyright © 2019 Basecamp, LLC
                 }
             }
             this.element.setAttribute("aria-expanded", newCollapsed ? "false" : "true");
-            this.controlledCollapsible().classList.toggle("is-collapsed", newCollapsed);
+            this.controlledCollapsible.classList.toggle("is-collapsed", newCollapsed);
         },
 
         connect: function () {
@@ -1920,13 +1934,18 @@ Copyright © 2019 Basecamp, LLC
                 this.element.addEventListener(e, this.listener);
             }, this);
 
-            if (this.isCheckable && this.element.type === "radio") {
+            if (this.isRadio) {
                 globalChangeListenerRequired(true);
             }
 
+            // synchronize state -- in all cases, this means setting the correct `aria-expanded`
+            // attribute; for checkable controls this also means setting the `is-collapsed` class
             this.element.setAttribute("aria-expanded", this.isCollapsed() ? "false" : "true");
             if (this.isCheckable) {
-                this.controlledCollapsible().classList.toggle("is-collapsed", this.isCollapsed);
+                var cc = this.controlledCollapsible;
+                if (cc) {
+                    cc.classList.toggle("is-collapsed", this.isCollapsed());
+                }
             }
         },
 
@@ -1935,7 +1954,7 @@ Copyright © 2019 Basecamp, LLC
                 this.element.removeEventListener(e, this.listener);
             }, this);
 
-            if (this.isCheckable && this.element.type === "radio") {
+            if (this.isRadio) {
                 globalChangeListenerRequired(false);
             }
         }
