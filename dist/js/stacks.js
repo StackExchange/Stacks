cbac463a4d5853e44ed1ab1a01ec8793b020f27d
Benjamin Hodgson <bhodgson@stackoverflow.com>
2019-04-01 16:35:21 -0400

    added JS for modal component

--- dist/js/stacks.js
+++ dist/js/stacks.js
@@ -1999,6 +1999,32 @@ Copyright © 2019 Basecamp, LLC
 
 ;
 
+(function() {
+    "use strict";
+
+    Stacks.addController("s-modal", {
+        targets: ["content"],
+
+        show: function() {
+            this.contentTarget.setAttribute("tabindex", 0);
+            this.contentTarget.setAttribute("aria-hidden", false);
+            document.body.setAttribute("tabindex", -1);
+            document.body.setAttribute("aria-hidden", true);
+        },
+        hide: function() {
+            this.contentTarget.setAttribute("tabindex", -1);
+            this.contentTarget.setAttribute("aria-hidden", true);
+            document.body.setAttribute("tabindex", 0);
+            document.body.setAttribute("aria-hidden", false);
+        },
+        nohide: function(event) {
+            event.stopPropagation();
+        }
+    });
+})();
+
+;
+
 (function () {
     "use strict";
     Stacks.addController("s-table", {
