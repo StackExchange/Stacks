703a726bea514d4c65d6133b21608c8ad21d7bb9
Benjamin Hodgson <bhodgson@stackoverflow.com>
2019-04-02 14:50:03 -0400

    close modals by escape key

--- dist/js/stacks.js
+++ dist/js/stacks.js
@@ -2008,17 +2008,24 @@ Copyright © 2019 Basecamp, LLC
         show: function() {
             this.contentTarget.setAttribute("tabindex", 0);
             this.contentTarget.setAttribute("aria-hidden", false);
+            this.contentTarget.focus();
             document.body.setAttribute("tabindex", -1);
             document.body.setAttribute("aria-hidden", true);
         },
         hide: function() {
             this.contentTarget.setAttribute("tabindex", -1);
             this.contentTarget.setAttribute("aria-hidden", true);
+            this.contentTarget.blur();
             document.body.setAttribute("tabindex", 0);
             document.body.setAttribute("aria-hidden", false);
         },
         nohide: function(event) {
             event.stopPropagation();
+        },
+        keydown: function(event) {
+            if (event.key === "Escape") {
+                this.hide();
+            }
         }
     });
 })();
