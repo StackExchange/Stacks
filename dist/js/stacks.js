b69e3581208755fd1c245f54fa81b332be5e4959
Ben Kelly <bkelly@stackoverflow.com>
2019-05-08 16:42:35 -0400

    correct toggle always showing the popover when called as an action add support for `placement` argument to pass to popper.js add support for hiding a popover when you click anywhere other than the toggle reference or the popover itself

--- dist/js/stacks.js
+++ dist/js/stacks.js
@@ -4620,22 +4620,45 @@ return Popper;
     Stacks.addController("s-popover", {
         targets: ["reference", "popover"],
         
-        initialize: function() {
+        connect: function() {
             this.popper = new Popper(this.referenceTarget, this.popoverTarget, {
-                placement: "auto"
+                placement: this.data.get("placement") || "bottom",
             });
+
+            // in order for removeEventListener to remove the right event, this bound function needs a constant reference
+            this._boundClickFn = this._hideOnOutsideClick.bind(this);
+
+            document.addEventListener("click", this._boundClickFn);
         },
 
-        toggle: function(show) {
-            this.popoverTarget.classList.toggle("is-visible", show);
+        disconnect: function() {
+            this.popper.destroy();
+
+            document.removeEventListener("click", this._boundClickFn);
+        },
+
+        toggle: function(event) {
+            this._toggle();
         },
 
         show: function() {
-            this.toggle(true);
+            this._toggle(true);
         },
 
         hide: function() {
-            this.toggle(false);
+            this._toggle(false);
+        },
+
+        _toggle: function(show) {
+            this.popoverTarget.classList.toggle("is-visible", show);
+        },
+
+        _hideOnOutsideClick: function(e) {
+            // check if the document was clicked inside either the reference element or the popover itself
+            // note: .contains also returns true if the node itself matches the target element
+            if (!this.referenceTarget.contains(e.target) && !this.popoverTarget.contains(e.target)) {
+                this.hide();
+            }
         }
     });
 })();
