9322977b603f378d7becf825f0e0ba18d965e687
Ben Kelly <bkelly@stackoverflow.com>
2019-07-18 17:11:48 -0400

    add s-popover.ts

--- lib/ts/controllers/s-popover.ts
+++ lib/ts/controllers/s-popover.ts
@@ -1,16 +1,26 @@
+
+
 (function () {
     "use strict";
-    Stacks.addController("s-popover", {
-        targets: [],
-        
+    Stacks.application.register("s-popover", class extends Stacks.StacksController {
+        static targets: [];
+
+        private referenceElement!: HTMLElement;
+        private popoverElement!: HTMLElement;
+        // @ts-ignore
+        private popper!: Popper;
+
+        private _boundClickFn!: any;
+        private _boundKeypressFn!: any;
+
         /**
          * Initializes popper.js and document events on controller connect
          */
-        connect: function() {
+        connect() {
             var referenceSelector = this.data.get("reference-selector");
 
             // if there is an alternative reference selector and that element exists, use it (otherwise use this element)
-            this.referenceElement = referenceSelector && this.element.querySelector(referenceSelector) || this.element;
+            this.referenceElement = referenceSelector && this.element.querySelector(referenceSelector) || <HTMLElement>this.element;
 
             var popoverId = this.referenceElement.getAttribute("aria-controls");
 
@@ -18,51 +28,59 @@
                 throw "[aria-controls=\"{POPOVER_ID}\"] required";
             }
 
-            this.popoverElement = document.getElementById(popoverId);
+            var element = document.getElementById(popoverId);
 
+            if (!element) {
+                throw "element with popover id not found";
+            }
+
+            this.popoverElement = element;
+
+            // @ts-ignore
             this.popper = new Popper(this.referenceElement, this.popoverElement, {
-                placement: this.data.get("placement") || "bottom",
+                // @ts-ignore
+                placement: this.data.get("placement") as Popper.Placement || "bottom",
             });
 
             // toggle classes based on if the popover is already visible
             this._toggleOptionalClasses(this.popoverElement.classList.contains("is-visible"));
-        },
+        };
 
         /**
          * Cleans up popper.js elements and disconnects all added event listeners on controller disconnect
          */
-        disconnect: function() {
+        disconnect() {
             this.popper.destroy();
             this._unbindDocumentEvents();
-        },
+        };
 
         /**
          * Toggles the visibility of the popover when called as a Stimulus action
          * @param {Event} event - The event object from the Stimulus action call
          */
-        toggle: function(event) {
+        toggle() {
             this._toggle();
-        },
+        };
 
         /**
          * Shows the popover
          */
-        show: function() {
+        show() {
             this._toggle(true);
-        },
+        };
 
         /**
          * Hides the popover
          */
-        hide: function() {
+        hide() {
             this._toggle(false);
-        },
+        };
 
         /**
          * Toggles the visibility of the popover element
          * @param {boolean=} show - Optional parameter that force shows/hides the element or toggles it if left undefined
          */
-        _toggle: function(show) {
+        _toggle(show?: boolean) {
             var toShow = show;
 
             // if we're letting the class toggle, we need to figure out if the popover is visible manually
@@ -86,45 +104,45 @@
 
             // shown/hidden events trigger after toggling the class
             this.triggerEvent(toShow ? "shown" : "hidden");
-        },
+        };
 
         /**
          * Binds global events to the document for hiding popovers on user interaction
          */
-        _bindDocumentEvents: function() {
+        _bindDocumentEvents() {
             // in order for removeEventListener to remove the right event, this bound function needs a constant reference
             this._boundClickFn = this._boundClickFn || this._hideOnOutsideClick.bind(this);
             this._boundKeypressFn = this._boundKeypressFn || this._hideOnEscapePress.bind(this);
 
             document.addEventListener("click", this._boundClickFn);
             document.addEventListener("keyup", this._boundKeypressFn);
-        },
+        };
 
         /**
          * Unbinds global events to the document for hiding popovers on user interaction
          */
-        _unbindDocumentEvents: function() {
+        _unbindDocumentEvents() {
             document.removeEventListener("click", this._boundClickFn);
             document.removeEventListener("keyup", this._boundKeypressFn);
-        },
+        };
 
         /**
          * Forces the popover to hide if a user clicks outside of it or its reference element
          * @param {Event} e - The document click event
          */
-        _hideOnOutsideClick: function(e) {
+        _hideOnOutsideClick(e: MouseEvent) {
             // check if the document was clicked inside either the reference element or the popover itself
             // note: .contains also returns true if the node itself matches the target element
-            if (!this.referenceElement.contains(e.target) && !this.popoverElement.contains(e.target)) {
+            if (!this.referenceElement.contains(<Node>e.target) && !this.popoverElement.contains(<Node>e.target)) {
                 this.hide();
             }
-        },
+        };
 
         /**
          * Forces the popover to hide if the user presses escape while it, one of its childen, or the reference element are focused
          * @param {Event} e - The document keyup event 
          */
-        _hideOnEscapePress: function(e) {
+        _hideOnEscapePress(e: KeyboardEvent) {
             // if the ESC key (27) wasn't pressed or if no popovers are showing, return
             if (e.which !== 27 || !this.popoverElement.classList.contains("is-visible")) {
                 return;
@@ -132,23 +150,23 @@
 
             // check if the target was inside the popover element and refocus the triggering element
             // note: .contains also returns true if the node itself matches the target element
-            if (this.popoverElement.contains(e.target)) {
+            if (this.popoverElement.contains(<Node>e.target)) {
                 this.referenceElement.focus();
             }
 
             this.hide();
-        },
+        };
 
         /**
          * Toggles all classes on the originating element based on the `class-toggle` data
          * @param {boolean=} show - Optional parameter that force shows/hides the classes or toggles them if left undefined 
          */
-        _toggleOptionalClasses: function(show) {
+        _toggleOptionalClasses(show?: boolean) {
             if (!this.data.has("toggle-class")) {
                 return;
             }
             var cl = this.referenceElement.classList;
-            this.data.get("toggle-class").split(/\s+/).forEach(function (cls) {
+            this.data.get("toggle-class")!.split(/\s+/).forEach(function (cls: string) {
                 cl.toggle(cls, show);
             });
         }
