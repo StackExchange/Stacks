38a8e9d260f080972bc5d1a57366fd6e78c9ba2f
Ham Vocke <hvocke@stackoverflow.com>
2019-08-22 14:59:03 +0200

    add delay, result-element data

--- lib/ts/controllers/s-autocomplete.ts
+++ lib/ts/controllers/s-autocomplete.ts
@@ -1,12 +1,12 @@
-
-
 (function () {
     "use strict";
     Stacks.application.register("s-autocomplete", class extends Stacks.StacksController {
-        static targets = [ "query" ];
+        static targets = ["query"];
 
         declare readonly queryTarget!: HTMLInputElement;
         declare readonly queryTargets!: HTMLInputElement[];
+
+        timeouts: number[] = [];
 
         connect() {
             console.log("Connected autocomplete");
@@ -17,14 +17,55 @@
         };
 
         /**
-         * Fires a query after a given delay
+         * Sends a query after a given delay
          */
-        runQuery() {
-            console.log("firing query", this.query)
+        query() {
+            let fun = () => {
+                console.log("firing query", this._query);
+                let response = "<div>1</div><div>2</div>";
+                this._resultElement.innerHTML = response;
+            };
+
+            // todo: debounce
+
+            let response = fun();
         }
 
-        get query() : string {
+        /**
+         * Get the entered search query
+         */
+        private get _query(): string {
             return this.queryTarget.value;
+        }
+
+        /**
+         * Get the specified delay in ms before firing a query
+         * Defaults to 250ms
+         */
+        private get _delay(): number {
+            return parseInt(this.data.get("delay") || "250");
+        }
+
+        /**
+         * Get the element which should display the autocomplete result
+         */
+        private get _resultElement(): Element {
+            let selector = this.data.get("result-element");
+            if (selector === null) {
+                throw "no result-element attribute given"
+            }
+
+            let element = document.querySelector(selector);
+            if (element === null) {
+                throw `no element found with selector ${element}`
+            }
+
+            return element;
+        }
+
+        private _debounce(fun: () => void, wait: number) {
+            this.timeouts.forEach(timeout => window.clearInterval(timeout));
+            this.timeouts.push(window.setTimeout(fun, wait));
         }
     });
 })();
