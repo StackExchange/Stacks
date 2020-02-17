b52cd4ab762a5b75cd1e774bc6af3a02f65cdd58
Aaron Shekey <aaron@aaronshekey.com>
2020-02-16 19:15:43 -0600

    Add a single product page

--- 11ty/.eleventy.js
+++ 11ty/.eleventy.js
@@ -1,5 +1,6 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addLayoutAlias('home', 'layouts/home.html');
+  eleventyConfig.addLayoutAlias('page', 'layouts/page.html');
 
   eleventyConfig.addPassthroughCopy('assets')
   return {
