8438b7a514073d789401bef95fa574a3bc088569
Aaron Shekey <aaron@aaronshekey.com>
2020-02-19 00:40:38 -0600

    Reference the link icon directly for now

--- docs/.eleventy.js
+++ docs/.eleventy.js
@@ -28,6 +28,8 @@ module.exports = function(eleventyConfig) {
   // Header shortcode
   eleventyConfig.addLiquidShortcode("header", function(tag, text) {
     var slug = text.replace(/\s+/g, '-').toLowerCase();
+    var fs = require("fs");
+    var linkIcon = fs.readFileSync("_includes/svg-icons/Link.svg").toString("utf-8");
 
     var output = '';
     output += '<div class="grid jc-space-between ai-end pe-none stacks-header">';
@@ -35,8 +37,7 @@ module.exports = function(eleventyConfig) {
     output +=     '<span class="pe-auto">' + text + '</span>';
     output +=   '</' + tag + '>';
     output +=   '<a class="grid grid__center mbn6 s-btn s-btn__muted pe-auto" href="#'+ slug +'">';
-    // TODO: We should use the icon shortcode within this shortcode to always keep these asset fresh
-    output +=     '<svg aria-hidden="true" class="svg-icon iconLink m0" width="18" height="18" viewBox="0 0 18 18"><path d="M7.22 11.83a6 6 0 0 0 1.62.85l.61-1.8a4.1 4.1 0 1 1 4.04-.8l1.26 1.42a6 6 0 1 0-7.53.33z"></path><path d="M10.65 6.23a6 6 0 0 0-1.6-.87L8.4 7.15a4.1 4.1 0 1 1-4.05.73L3.12 6.43a6 6 0 1 0 7.53-.2z"></path></svg>';
+    output +=     linkIcon;
     output +=   '</a>';
     output += '</div>';
 
