e8464c92a324e7b8b354216909af9398bfe50d3f
Ben Kelly <b-kelly@users.noreply.github.com>
2020-02-29 18:42:16 -0500

    copy docs from old PR and port to 11ty... sure hope I didn't erase any intermediary changes ;)

--- lib/ts/controllers/s-modal.ts
+++ lib/ts/controllers/s-modal.ts
@@ -1,129 +1,131 @@
-class ModalController extends Stacks.StacksController {
-    static targets = ["modal"];
+namespace Stacks {
+        
+    export class ModalController extends Stacks.StacksController {
+        static targets = ["modal"];
 
-    // TODO should Stimulus bind this? Needed to keep TS error-free
-    private modalTarget!: HTMLElement;
+        // TODO should Stimulus bind this? Needed to keep TS error-free
+        private modalTarget!: HTMLElement;
 
-    private _boundClickFn!: any;
-    private _boundKeypressFn!: any;
+        private _boundClickFn!: any;
+        private _boundKeypressFn!: any;
 
-    connect () { }
+        connect () { }
 
-    /**
-     * Disconnects all added event listeners on controller disconnect
-     */
-    disconnect() {
-        this._unbindDocumentEvents();
-    };
-
-    /**
-     * Toggles the visibility of the modal
-     */
-    toggle () {
-        this._toggle();
-    }
-
-    /**
-     * Shows the modal
-     */
-    show () {
-        this._toggle(true);
-    }
-
-    /**
-     * Hides the modal
-     */
-    hide () {
-        this._toggle(false);
-    }
-
-    /**
-     * Toggles the visibility of the modal element
-     * @param show Optional parameter that force shows/hides the element or toggles it if left undefined
-     */
-    _toggle (show?: boolean | undefined) {
-        var toShow = show;
-
-        // if we're letting the class toggle, we need to figure out if the popover is visible manually
-        if (typeof toShow === "undefined") {
-            toShow = this.modalTarget.getAttribute("aria-hidden") === "true";
-        }
-
-        // show/hide events trigger before toggling the class
-        this.triggerEvent(toShow ? "show" : "hide");
-
-        this.modalTarget.setAttribute("aria-hidden", toShow ? "false" : "true");
-
-        if (toShow) {
-            this._bindDocumentEvents();
-        }
-        else {
+        /**
+         * Disconnects all added event listeners on controller disconnect
+         */
+        disconnect() {
             this._unbindDocumentEvents();
+        };
+
+        /**
+         * Toggles the visibility of the modal
+         */
+        toggle () {
+            this._toggle();
         }
 
-        // shown/hidden events trigger after toggling the class
-        this.triggerEvent(toShow ? "shown" : "hidden");
-    }
+        /**
+         * Shows the modal
+         */
+        show () {
+            this._toggle(true);
+        }
 
-    /**
-     * Binds global events to the document for hiding popovers on user interaction
-     */
-    _bindDocumentEvents () {
-        // in order for removeEventListener to remove the right event, this bound function needs a constant reference
-        this._boundClickFn = this._boundClickFn || this._hideOnOutsideClick.bind(this);
-        this._boundKeypressFn = this._boundKeypressFn || this._hideOnEscapePress.bind(this);
+        /**
+         * Hides the modal
+         */
+        hide () {
+            this._toggle(false);
+        }
 
-        document.addEventListener("click", this._boundClickFn);
-        document.addEventListener("keyup", this._boundKeypressFn);
-    }
+        /**
+         * Toggles the visibility of the modal element
+         * @param show Optional parameter that force shows/hides the element or toggles it if left undefined
+         */
+        _toggle (show?: boolean | undefined) {
+            var toShow = show;
 
-    /**
-     * Unbinds global events to the document for hiding popovers on user interaction
-     */
-    _unbindDocumentEvents () {
-        document.removeEventListener("click", this._boundClickFn);
-        document.removeEventListener("keyup", this._boundKeypressFn);
-    }
+            // if we're letting the class toggle, we need to figure out if the popover is visible manually
+            if (typeof toShow === "undefined") {
+                toShow = this.modalTarget.getAttribute("aria-hidden") === "true";
+            }
 
-    /**
-     * Forces the popover to hide if a user clicks outside of it or its reference element
-     */
-    _hideOnOutsideClick (e: Event) {
-        var target = <Node>e.target;
-        // check if the document was clicked inside either the toggle element or the modal itself
-        // note: .contains also returns true if the node itself matches the target element
-        //TODO change !. to ?. after TS 3.7 launches
-        if (!this.modalTarget.querySelector(".s-modal--dialog")!.contains(target)) {
+            // show/hide events trigger before toggling the class
+            this.triggerEvent(toShow ? "show" : "hide");
+
+            this.modalTarget.setAttribute("aria-hidden", toShow ? "false" : "true");
+
+            if (toShow) {
+                this._bindDocumentEvents();
+            }
+            else {
+                this._unbindDocumentEvents();
+            }
+
+            // shown/hidden events trigger after toggling the class
+            this.triggerEvent(toShow ? "shown" : "hidden");
+        }
+
+        /**
+         * Binds global events to the document for hiding popovers on user interaction
+         */
+        _bindDocumentEvents () {
+            // in order for removeEventListener to remove the right event, this bound function needs a constant reference
+            this._boundClickFn = this._boundClickFn || this._hideOnOutsideClick.bind(this);
+            this._boundKeypressFn = this._boundKeypressFn || this._hideOnEscapePress.bind(this);
+
+            document.addEventListener("click", this._boundClickFn);
+            document.addEventListener("keyup", this._boundKeypressFn);
+        }
+
+        /**
+         * Unbinds global events to the document for hiding popovers on user interaction
+         */
+        _unbindDocumentEvents () {
+            document.removeEventListener("click", this._boundClickFn);
+            document.removeEventListener("keyup", this._boundKeypressFn);
+        }
+
+        /**
+         * Forces the popover to hide if a user clicks outside of it or its reference element
+         */
+        _hideOnOutsideClick (e: Event) {
+            var target = <Node>e.target;
+            // check if the document was clicked inside either the toggle element or the modal itself
+            // note: .contains also returns true if the node itself matches the target element
+            //TODO change !. to ?. after TS 3.7 launches
+            if (!this.modalTarget.querySelector(".s-modal--dialog")!.contains(target)) {
+                this._toggle(false);
+            }
+        }
+
+        /**
+         * Forces the popover to hide if the user presses escape while it, one of its childen, or the reference element are focused
+         */
+        _hideOnEscapePress (e: KeyboardEvent) {
+            // if the ESC key (27) wasn't pressed or if no popovers are showing, return
+            if (e.which !== 27 || this.modalTarget.getAttribute("aria-hidden") === "true") {
+                return;
+            }
+
             this._toggle(false);
         }
     }
 
     /**
-     * Forces the popover to hide if the user presses escape while it, one of its childen, or the reference element are focused
+     * Helper to manually show an s-modal element via external JS
+     * @param element the element the `data-controller="s-modal"` attribute is on
+     * @param show whether to force show/hide the modal; toggles the modal if left undefined
      */
-    _hideOnEscapePress (e: KeyboardEvent) {
-        // if the ESC key (27) wasn't pressed or if no popovers are showing, return
-        if (e.which !== 27 || this.modalTarget.getAttribute("aria-hidden") === "true") {
-            return;
+    export function toggleModal(element: HTMLElement, show?: boolean | undefined) {
+        var controller = Stacks.application.getControllerForElementAndIdentifier(element, "s-modal") as ModalController;
+
+        if (!controller) {
+            throw "Unable to get s-modal controller from element";
         }
 
-        this._toggle(false);
+        controller._toggle(show);
     }
 }
-
-/**
- * Helper to manually show an s-modal element via external JS
- * @param element the element the `data-controller="s-modal"` attribute is on
- * @param show whether to force show/hide the modal; toggles the modal if left undefined
- */
-export function toggleModal(element: HTMLElement, show?: boolean | undefined) {
-    var controller = Stacks.application.getControllerForElementAndIdentifier(element, "s-modal") as ModalController;
-
-    if (!controller) {
-        throw "Unable to get s-modal controller from element";
-    }
-
-    controller._toggle(show);
-}
-
-Stacks.application.register("s-modal", ModalController);
+Stacks.application.register("s-modal", Stacks.ModalController);
