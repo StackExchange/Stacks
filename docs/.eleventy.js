72f54b323de6c593a762edf833f5ebd445630c70
Aaron Shekey <aaron@aaronshekey.com>
2020-08-06 16:45:33 -0500

    Rename .grid to .d-flex

--- docs/.eleventy.js
+++ docs/.eleventy.js
@@ -60,11 +60,11 @@ module.exports = function(eleventyConfig) {
     var linkIcon = Icons["Link"];
 
     var output = '';
-    output += '<div class="grid jc-space-between ai-end pe-none stacks-header">';
+    output += '<div class="d-flex jc-space-between ai-end pe-none stacks-header">';
     output +=   '<' + tag + ' class="flex--cell fl1 stacks-' + tag + '" id="'+ slug +'">';
     output +=     '<span class="pe-auto">' + text + '</span>';
     output +=   '</' + tag + '>';
-    output +=   '<a class="grid flex__center mbn6 s-btn s-btn__muted pe-auto" href="#'+ slug +'">';
+    output +=   '<a class="d-flex flex__center mbn6 s-btn s-btn__muted pe-auto" href="#'+ slug +'">';
     output +=     linkIcon;
     output +=   '</a>';
     output += '</div>';
