b425f4f93d4dcb3f174e31f31c2389938e53438f
Aaron Shekey <aaron@aaronshekey.com>
2021-01-07 11:35:37 -0600

    Grab a fake package, throw it into node_modules, and setup all the copy tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -118,7 +118,7 @@ module.exports = function(grunt) {
 
             stacks_js: {
                 files: ['lib/ts/**/*.ts'], // note: this doesn't watch any of the npm dependencies
-                tasks: ['concurrent:compile_stacks_js', 'copy:js2docs']
+                tasks: ['concurrent:compile_stacks_js', 'copy:js2docs', 'copy:editorJs', 'copy:editorCSS']
             },
         },
         // Run tasks in parallel
@@ -136,7 +136,7 @@ module.exports = function(grunt) {
             compile: [
                 'concurrent:compile_stacks_css',
                 'ts:docs_js',
-                ['concurrent:compile_stacks_js', 'copy:js2docs']
+                ['concurrent:compile_stacks_js', 'copy:js2docs', 'copy:editorJs', 'copy:editorCSS']
             ],
 
             // Stacks JS
@@ -166,6 +166,24 @@ module.exports = function(grunt) {
                 dest: 'docs/assets/js/',
                 flatten: true,
                 expand: true
+            },
+            editorJs: {
+                src: 'node_modules/@stackoverflow/stacks-editor/dist/app.bundle.js',
+                dest: 'docs/assets/js/',
+                flatten: true,
+                expand: true,
+                rename: function(dest, src) {
+                    return dest + src.replace('app.bundle.js','library.stacks-editor.js');
+                }
+            },
+            editorCSS: {
+                src: 'node_modules/@stackoverflow/stacks-editor/dist/browser.css',
+                dest: 'docs/assets/css/',
+                flatten: true,
+                expand: true,
+                rename: function(dest, src) {
+                    return dest + src.replace('browser.css','stacks-editor.css');
+                }
             },
             declarations: {
                 expand: true,
