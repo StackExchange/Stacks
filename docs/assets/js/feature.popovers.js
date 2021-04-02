a619ced22dabf7109c73315363ffc5a84b9a4408
Aaron Shekey <aaron@aaronshekey.com>
2021-04-02 16:04:16 -0500

    Move the JS examples to feature.popovers.js
    This satisfies our weird JS-based navigation

--- docs/assets/js/feature.popovers.js
+++ docs/assets/js/feature.popovers.js
@@ -1,4 +1,23 @@
 $(document).ready(function() {
     Stacks.setTooltipText(document.querySelector(".js-text-tooltip-example"), "Only members of this Team can see the information posted here. It will never be shared publicly or accessible outside of your Team.", { placement: "top-start" });
     Stacks.setTooltipHtml(document.querySelector(".js-html-tooltip-example"), "View all questions with <span class='s-tag'>stacks</span>", { placement: "top-end" });
+
+    window.addEventListener("load", () => {
+        Stacks.addController("section", {
+            targets: ["help"],
+            showHelp(e) { Stacks.showPopover(this.helpTarget); e.stopPropagation(); },
+            hideHelp(e) { Stacks.hidePopover(this.helpTarget); },
+        });
+
+        Stacks.addController("actions", {
+            loaded: false,
+            load() {
+                if (this.loaded) { return; }
+                setTimeout(() => {
+                    Stacks.attachPopover(this.element, `<div class="s-popover wmx1"><div class="s-popover--arrow"></div><div class="s-btn d-block">Edit</div><div class="s-btn s-btn__danger d-block">Delete</div></div>`, { autoShow: true, toggleOnClick: true });
+                    this.loaded = true;
+                });
+            },
+        });
+    });
 });
