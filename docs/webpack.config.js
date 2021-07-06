6528f129fa2a832eceb5887dda538de3e7294ed7
b-kelly <b-kelly@users.noreply.github.com>
2021-07-06 17:16:03 -0400

    load each entry.*.js file as its own bundle

--- docs/webpack.config.js
+++ docs/webpack.config.js
@@ -1,14 +1,26 @@
+const fs = require("fs");
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = (_, argv) => {
     const isProd = argv.mode === "production"
+
+    // load each entry.*.js file in assets/js as its own bundle
+    const entries = fs.readdirSync(path.resolve(__dirname, "assets/js/"))
+        .filter(f => f.startsWith("entry."))
+        .reduce((p, n) => {
+            // { "entry.file": "path/to/entry.file.js" }
+            p[n.slice(0, -3)] = path.resolve(__dirname, "assets/js/", n);
+            return p;
+        }, {});
+
     return {
         mode: isProd ? "production" : "development",
         devtool: isProd ? false : "inline-source-map",
         entry: {
             docs: path.resolve(__dirname, "assets/js/index.ts"),
+            ...entries
         },
         output: {
             filename: "[name].js",
