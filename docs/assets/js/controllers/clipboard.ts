05ae67e2ed91a3c5a858469d975c39c29b7bedf9
Dan Cormier <dcormier@stackoverflow.com>
2022-07-06 18:06:33 -0400

    use stacks application.register
    addresses https://github.com/StackExchange/Stacks/pull/883#discussion_r835332259

--- docs/assets/js/controllers/clipboard.ts
+++ docs/assets/js/controllers/clipboard.ts
@@ -1,41 +1,39 @@
-import { Application, Controller } from "stimulus";
+import * as Stacks from '../../../../lib/ts/stacks';
 
-(function(){
-    Application.start().register("clipboard", class extends Controller {
-        static targets = ["source"];
-        sourceTarget!: HTMLElement;
+Stacks.application.register("clipboard", class extends Stacks.StacksController {
+    static targets = ["source"];
+    sourceTarget!: HTMLElement;
 
-        connect() {
-            super.connect();
-        };
+    connect() {
+        super.connect();
+    };
 
-        copy() {
-            const text = this.sourceTarget.innerText;
-            navigator.clipboard.writeText(text);
-            this.handleVisible();
-        }
+    copy() {
+        const text = this.sourceTarget.innerText;
+        navigator.clipboard.writeText(text);
+        this.handleVisible();
+    }
 
-        private handleVisible() {
-            const { scope } = this.targets;
+    private handleVisible() {
+        const { scope } = this.targets;
 
-            const hideElements = scope.findAllElements('[data-hide-on-copy]');
-            const showElements = scope.findAllElements('[data-show-on-copy]');
+        const hideElements = scope.findAllElements('[data-hide-on-copy]');
+        const showElements = scope.findAllElements('[data-show-on-copy]');
 
+        hideElements.forEach(el => {
+            el.classList.add("d-none");
+        });
+        showElements.forEach(el => {
+            el.classList.remove("d-none");
+        });
+
+        setTimeout(function () {
             hideElements.forEach(el => {
-                el.classList.add("d-none");
-            });
-            showElements.forEach(el => {
                 el.classList.remove("d-none");
             });
-
-            setTimeout(function () {
-                hideElements.forEach(el => {
-                    el.classList.remove("d-none");
-                });
-                showElements.forEach(el => {
-                    el.classList.add("d-none");
-                });
-            }, 3000);
-        }
-    });
-})();
+            showElements.forEach(el => {
+                el.classList.add("d-none");
+            });
+        }, 3000);
+    }
+});
