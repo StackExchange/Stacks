da47350c3e7ab1e14dabab6c97157534f3c23807
Giamir Buoncristiani <giamir.buoncristiani@gmail.com>
2022-11-28 10:45:10 +0100

    fix(format): fix all prettier violations
    exclude less files from prettier formatting since running the formatter was breaking the syntax

--- lib/ts/stacks.ts
+++ lib/ts/stacks.ts
@@ -3,25 +3,31 @@ import * as Stimulus from "stimulus";
 export class StacksApplication extends Stimulus.Application {
     static _initializing = true;
 
-    load(...definitions: Stimulus.Definition[]): void
-    load(definitions: Stimulus.Definition[]): void
-    load(head: Stimulus.Definition | Stimulus.Definition[], ...rest: Stimulus.Definition[]) {
+    load(...definitions: Stimulus.Definition[]): void;
+    load(definitions: Stimulus.Definition[]): void;
+    load(
+        head: Stimulus.Definition | Stimulus.Definition[],
+        ...rest: Stimulus.Definition[]
+    ) {
         const definitions = Array.isArray(head) ? head : [head, ...rest];
 
         for (const definition of definitions) {
             const hasPrefix = /^s-/.test(definition.identifier);
             if (StacksApplication._initializing && !hasPrefix) {
-                throw "Stacks-created Stimulus controller names must start with \"s-\".";
+                throw 'Stacks-created Stimulus controller names must start with "s-".';
             }
             if (!StacksApplication._initializing && hasPrefix) {
-                throw "The \"s-\" prefix on Stimulus controller names is reserved for Stacks-created controllers.";
+                throw 'The "s-" prefix on Stimulus controller names is reserved for Stacks-created controllers.';
             }
         }
 
         super.load(definitions);
     }
 
-    static start(element?: Element, schema?: Stimulus.Schema): StacksApplication {
+    static start(
+        element?: Element,
+        schema?: Stimulus.Schema
+    ): StacksApplication {
         const application = new StacksApplication(element, schema);
         // eslint-disable-next-line @typescript-eslint/no-floating-promises
         application.start();
@@ -38,18 +44,26 @@ export const application: Stimulus.Application = StacksApplication.start();
 export class StacksController extends Stimulus.Controller {
     protected getElementData(element: Element, key: string) {
         return element.getAttribute("data-" + this.identifier + "-" + key);
-    };
+    }
     protected setElementData(element: Element, key: string, value: string) {
         element.setAttribute("data-" + this.identifier + "-" + key, value);
-    };
+    }
     protected removeElementData(element: Element, key: string) {
         element.removeAttribute("data-" + this.identifier + "-" + key);
-    };
-    protected triggerEvent<T>(eventName: string, detail?: T, optionalElement?: Element) {
+    }
+    protected triggerEvent<T>(
+        eventName: string,
+        detail?: T,
+        optionalElement?: Element
+    ) {
         const namespacedName = this.identifier + ":" + eventName;
-        let event : CustomEvent<T>;
+        let event: CustomEvent<T>;
         try {
-            event = new CustomEvent(namespacedName, {bubbles: true, cancelable: true, detail: detail});
+            event = new CustomEvent(namespacedName, {
+                bubbles: true,
+                cancelable: true,
+                detail: detail,
+            });
         } catch (ex) {
             // Internet Explorer
             // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
@@ -58,24 +72,30 @@ export class StacksController extends Stimulus.Controller {
         }
         (optionalElement || this.element).dispatchEvent(event);
         return event;
-    };
+    }
 }
 
 // ControllerDefinition/createController/addController is here to make
 // it easier to consume Stimulus from ES5 files (without classes)
-export interface ControllerDefinition { 
+export interface ControllerDefinition {
     [name: string]: unknown;
     targets?: string[];
 }
-export function createController(controllerDefinition: ControllerDefinition): typeof StacksController {
+export function createController(
+    controllerDefinition: ControllerDefinition
+): typeof StacksController {
     // eslint-disable-next-line no-prototype-builtins
     const Controller = controllerDefinition.hasOwnProperty("targets")
-        ? class Controller extends StacksController { static targets = controllerDefinition.targets ?? []}
+        ? class Controller extends StacksController {
+              static targets = controllerDefinition.targets ?? [];
+          }
         : class Controller extends StacksController {};
 
     for (const prop in controllerDefinition) {
         // eslint-disable-next-line no-prototype-builtins
-        const ownPropDescriptor = controllerDefinition.hasOwnProperty(prop) && Object.getOwnPropertyDescriptor(controllerDefinition, prop);
+        const ownPropDescriptor =
+            controllerDefinition.hasOwnProperty(prop) &&
+            Object.getOwnPropertyDescriptor(controllerDefinition, prop);
         if (prop !== "targets" && ownPropDescriptor) {
             Object.defineProperty(
                 Controller.prototype,
