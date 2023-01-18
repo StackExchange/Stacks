3f7f78207e9a8e8001170214412a5081da66330c
Dan Cormier <dcormier@stackoverflow.com>
2023-01-18 11:01:45 -0500

    feat(notice): add show/hide controller (#1216)
    * feat(notice): add show/hide controller
    
    * Add auto-hiding to the controller
    
    * Make auto-hide opt-in
    
    * Replace banner demo with controller
    
    For the record, I don't know if this is a good fit. We probably should consolidate these components first, then port the new controller to work in all sorts of contexts.
    
    * Remove console logs
    
    * Create a banner-specific controller
    
    * Turn generalized notice controller into toast controller
    
    * I'll show you around this alphabet town
    
    * Add focus capture to toast controller
    
    * Move example toast
    
    * Fix banner icon
    
    * Moving the toast back for diff purposes
    
    * Register banner controller
    
    * Add a banner controller test
    
    It doesn't work atm. It will… hopefully.
    
    * Update s-banner.test.ts
    
    * cleanup
    
    * Minor cleanup
    
    * Update docs w/ JS and inline examples
    
    * Change bannerTarget to a readonly declaration
    
    h/t @giamir https://github.com/StackExchange/Stacks/pull/1216#issuecomment-1381824710
    
    * Add test for hideBanner()
    
    * Add visual tests for each banner variant
    
    * Add baseline images for banner visual tests
    
    * Update notice docs with toast controller
    
    * Update hideBanner, showBanner import
    
    * Change toast declarations to a readonly
    
    * Add toast unit tests
    
    * Add toast visual tests; update banner visual tests

---
+++ lib/test/s-toast.test.ts
@@ -0,0 +1,64 @@
+import { html, fixture, expect } from "@open-wc/testing";
+import { screen } from "@testing-library/dom";
+import userEvent from "@testing-library/user-event";
+import "../ts/index";
+import { showToast, hideToast } from "../ts/index";
+
+const user = userEvent.setup();
+
+const testToast = (hidden = true) => html`
+<button
+    class="js-open-test-toast"
+    data-toggle="s-toast"
+    data-target="#test-toast">
+    Show test toast
+</button>
+<button type="button" aria-label="Dismiss">
+    Close toast
+</button>
+<div
+    role="alertdialog"
+    id="test-toast"
+    class="s-toast"
+    aria-hidden="${hidden}"
+    aria-labelledby="toast-message"
+    data-controller="s-toast"
+    data-s-toast-target="toast"
+    data-s-toast-return-element=".js-open-test-toast[data-target='#test-toast']"
+    data-testid="test-toast">
+    <aside class="s-notice s-notice__success">
+        <div id="notice-message">Test toast</div>
+    </aside>
+</div>`;
+
+describe("s-toast", () => {
+    it("trigger should make toast visible", async () => {
+        await fixture(testToast(true));
+
+        const button = screen.getAllByRole("button")[0]; // Trigger button
+        const toast = screen.getByTestId("test-toast");
+
+        expect(toast).to.have.attribute('aria-hidden', 'true');
+        button.addEventListener('click', () => showToast(toast));
+
+        await user.click(button);
+        expect(toast).to.have.attribute('aria-hidden', 'false');
+    });
+
+    it("trigger should hide toast", async () => {
+        await fixture(testToast(false));
+
+        const button = screen.getAllByRole("button")[1];  // Close button
+        const toast = screen.getByTestId("test-toast");
+
+        expect(toast).to.have.attribute('aria-hidden', 'false');
+        button.addEventListener('click', () => hideToast(toast));
+
+        await user.click(button);
+        expect(toast).to.have.attribute('aria-hidden', 'true');
+    });
+
+    // TODO: add test for refocus trigger
+
+    // TODO: add test for removing toast from DOM
+});
