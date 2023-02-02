02c1d2ccaedff35ce3cb2b1f9f440bd3ad160d9c
Dan Cormier <dcormier@stackoverflow.com>
2023-02-02 12:55:47 -0500

    Uncomment s-btn vis regression test

--- lib/test/s-btn.visual.test.ts
+++ lib/test/s-btn.visual.test.ts
@@ -1,12 +1,12 @@
-// import { html, fixture } from "@open-wc/testing";
-// import { visualDiff } from "@web/test-runner-visual-regression";
-// import "../ts/index";
+import { html, fixture } from "@open-wc/testing";
+import { visualDiff } from "@web/test-runner-visual-regression";
+import "../ts/index";
 
-// describe("s-btn", () => {
-//     it("should not introduce visual regressions for loading button", async () => {
-//         const btn = await fixture(html`
-//             <button class="s-btn is-loading" type="button">Loading</button>
-//         `);
-//         await visualDiff(btn, "s-btn-is-loading");
-//     });
-// });
+describe("s-btn", () => {
+    it("should not introduce visual regressions for loading button", async () => {
+        const btn = await fixture(html`
+            <button class="s-btn is-loading" type="button">Loading</button>
+        `);
+        await visualDiff(btn, "s-btn-is-loading");
+    });
+});
