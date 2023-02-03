ef7a550300ecfc769262666d57da1371ea3fe89f
Dan Cormier <dcormier@stackoverflow.com>
2023-02-03 12:48:57 -0500

    test(btn): resolve .is-loading test flakiness (#1256)
    * test(btn): renable .is-loading vis test
    
    * Rename s-btn test file
    
    * Uncomment s-btn vis regression test
    
    * Try padded span wrapper
    
    * Add fixed height/width wrapper
    
    As suggested in https://github.com/StackExchange/Stacks/pull/1256#issuecomment-1415901446
    
    * Tweak dimensions
    
    * update baselines

--- lib/test/s-btn.visual.test.ts
+++ lib/test/s-btn.visual.test.ts
@@ -1,12 +1,16 @@
-// import { html, fixture } from "@open-wc/testing";
-// import { visualDiff } from "@web/test-runner-visual-regression";
-// import "../ts/index";
+import { html, fixture } from "@open-wc/testing";
+import { visualDiff } from "@web/test-runner-visual-regression";
+import "../ts/index";
 
-// describe("s-btn", () => {
-//     it("should not introduce visual regressions for loading button", async () => {
-//         const btn = await fixture(html`
-//             <button class="s-btn is-loading" type="button">Loading</button>
-//         `);
-//         await visualDiff(btn, "s-btn-is-loading");
-//     });
-// });
+describe("s-btn", () => {
+    it("should not introduce visual regressions for loading button", async () => {
+        // Adding a padded wrapper to avoid GitHub Actions diff discrepancies
+        const btn = await fixture(html`
+            <div style="height: 38px; width: 88px; display: inline-block;">
+                <button class="s-btn is-loading" type="button">Loading</button>
+            </div>
+        `);
+
+        await visualDiff(btn, "s-btn-is-loading");
+    });
+});
