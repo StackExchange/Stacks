8c6fd19ea84842754aaade4a6fcc446bf06e882a
Dan Cormier <dcormier@stackoverflow.com>
2023-02-15 16:04:23 -0500

    Add TODO, examples for makeTests fn

--- lib/ts/test-utils.ts
+++ lib/ts/test-utils.ts
@@ -10,6 +10,53 @@ const attrObjToString = (attrs: Record<string, string>): string => {
 
 export const buildTestid = (arr: string[]) => arr.filter(Boolean).join("-");
 
+// TODO: create `makeTests` function to generate tests for all variants and modifiers
+// s-avatar example:
+// makeTests({
+//     baseClass: "s-avatar",
+//     variants: null,
+//     modifiers: {
+//         basePrimary: ["24", "32", "48", "64", "96", "128"],
+//     },
+//     children: [
+//         `<img
+//             class="s-avatar--image"
+//             src="https://picsum.photos/48"
+//             alt="team logo"
+//         />`,
+//         `<div class="s-avatar--letter" aria-hidden="true">S</div>`,
+//     ],
+//     options: {
+//         testColorThemes: true,
+//         testHighContrast: true,
+//         includeNullVariant: false,
+//         includeNullModifier: true,
+//     },
+// });
+// 
+// s-btn example:
+// makeTests({
+//     baseClass: "s-btn",
+//     variants: ["danger", "muted", "primary"],
+//     modifiers: {
+//         basePrimary: [
+//             ...["xs", "sm", "md"],
+//             ...["link", "unset"],
+//             ...["facebook", "github", "google"],
+//         ],
+//         baseSecondary: ["dropdown", "icon"],
+//         global: ["is-loading"],
+//     },
+//     children: [
+//         `Ask question`,
+//         `
+//             Ask question
+//             <span class="s-btn--badge">
+//                 <span class="s-btn--number">198</span>
+//             </span>
+//         `,
+//     ],
+// });
 
 export const makeTest = ({
     attributes = {},
