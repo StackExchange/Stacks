03ea55a79121d24e5c625a7049867a78c8dd7fce
Giamir Buoncristiani <giamir.buoncristiani@gmail.com>
2023-03-24 16:10:38 +0100

    ci(reporter): cleanup custom reporter

--- web-test-runner.config.ci.mjs
+++ web-test-runner.config.ci.mjs
@@ -1,88 +1,101 @@
-// TODO: this needs some cleanup - as of now it's a bit of a copy paste from the summary and default reporter
-// custom reporter to better differentiate between skipped from failing tests and log tests failures stack trace correctly
 import { reportTestsErrors } from "./node_modules/@web/test-runner/dist/reporter/reportTestsErrors.js";
 import { reportTestFileErrors } from "./node_modules/@web/test-runner/dist/reporter/reportTestFileErrors.js";
 import { reportBrowserLogs } from "./node_modules/@web/test-runner/dist/reporter/reportBrowserLogs.js";
 import { TestRunnerLogger } from "./node_modules/@web/test-runner/dist/logger/TestRunnerLogger.js";
+import { yellow, green, red, dim } from "nanocolors";
 
-const color =
-    ([x, y]) =>
-    (z) =>
-        `\x1b[${x}m${z}\x1b[${y}m${reset}`;
-const reset = `\x1b[0m\x1b[0m`;
-const green = color([32, 89]);
-const red = color([31, 89]);
-const yellow = color([33, 89]);
-const dim = color([2, 0]);
+import defaultConfig from "./web-test-runner.config.mjs";
 
-function customReporter(opts) {
-    const { flatten = false } = opts ?? {};
-    let args;
-    let favoriteBrowser;
+// Custom reporter for CI highlighting exclusively skipped and failing tests
+// It prints out stack traces for failures
+function ciReporter() {
+    let browserNames;
 
     const logger = new TestRunnerLogger();
 
-    function log(logger, name, passed, skipped, prefix = "  ", postfix = "") {
-        let sign;
-        if (skipped) {
-            sign = yellow("o");
-        } else {
-            sign = passed ? green("✓") : red("𐄂");
-        }
-        if (flatten) logger.log(`${sign} ${prefix} ${name}${postfix}`);
-        else logger.log(`${prefix}  ${sign} ${name}`);
+    function log(logger, name, passed, skipped) {
+        const sign = skipped ? yellow("○") : passed ? green("✓") : red("𐄂");
+        logger.log(`    ${sign} ${name}`);
     }
 
     function logResults(logger, results, prefix, browser) {
         const browserName = browser?.name ? ` ${dim(`[${browser.name}]`)}` : "";
-        for (const result of results?.tests ?? []) {
-            log(
-                logger,
-                result.name,
-                result.passed,
-                result.skipped,
-                prefix,
-                browserName
-            );
+        for (const result of results?.tests.filter((r) => !r.passed) ?? []) {
+            log(logger, result.name, result.passed, result.skipped);
         }
 
         for (const suite of results?.suites ?? []) {
-            logSuite(logger, suite, prefix, browser);
+            const suitePassed = suite.tests.every((t) => t.passed);
+            if (!suitePassed) {
+                logSuite(logger, suite, prefix, browser);
+                logger.log("");
+            }
         }
     }
 
     function logSuite(logger, suite, parent, browser) {
         const browserName = browser?.name ? ` ${dim(`[${browser.name}]`)}` : "";
-        let pref = parent ? `${parent} ` : "";
-        if (flatten) pref += `${suite.name}`;
-        else logger.log(`${pref}${suite.name}${browserName}`);
-
+        const pref = parent ? `${parent} ` : "";
+        logger.log(`${pref}${suite.name}${browserName}`);
         logResults(logger, suite, pref, browser);
     }
 
+    function sumStats(a, b) {
+        return {
+            passed: a.passed + b.passed,
+            failed: a.failed + b.failed,
+            skipped: a.skipped + b.skipped,
+        };
+    }
+
+    function resultsSummary(results) {
+        return results.suites.reduce(
+            (a, s) => {
+                const sSummary = resultsSummary(s);
+                return sumStats(a, sSummary);
+            },
+            results.tests.reduce(
+                (a, t) => {
+                    if (t.skipped) {
+                        a.skipped += 1;
+                    } else if (t.passed) {
+                        a.passed += 1;
+                    } else {
+                        a.failed += 1;
+                    }
+                    return a;
+                },
+                { passed: 0, failed: 0, skipped: 0 }
+            )
+        );
+    }
+
     return {
-        start(_args) {
-            args = _args;
-            favoriteBrowser =
-                args.browserNames.find((name) => {
-                    const n = name.toLowerCase();
-                    return (
-                        n.includes("chrome") ||
-                        n.includes("chromium") ||
-                        n.includes("firefox")
-                    );
-                }) ?? args.browserNames[0];
+        start(args) {
+            browserNames = args.browserNames;
         },
 
         reportTestFileResults({ sessionsForTestFile }) {
             for (const session of sessionsForTestFile) {
                 logResults(logger, session.testResults, "", session.browser);
-                logger.log("");
             }
             reportBrowserLogs(logger, sessionsForTestFile);
         },
 
         onTestRunFinished({ sessions }) {
+            const summary = sessions.reduce(
+                (a, s) => sumStats(a, resultsSummary(s.testResults)),
+                { passed: 0, failed: 0, skipped: 0 }
+            );
+
+            logger.log(green(`✓ Passed: ${summary.passed}`));
+            if (summary.skipped > 0) {
+                logger.log(yellow(`○ Skipped: ${summary.skipped}`));
+            }
+            if (summary.failed > 0) {
+                logger.log(red(`x Failed: ${summary.failed}`));
+            }
+
             const failedSessions = sessions.filter(
                 (s) => !s.skipped && !s.passed
             );
@@ -90,14 +103,14 @@ function customReporter(opts) {
                 logger.log("\n\nErrors Reported in Tests:\n\n");
                 reportTestsErrors(
                     logger,
-                    args.browserNames,
-                    favoriteBrowser,
+                    browserNames,
+                    "Chromium",
                     failedSessions
                 );
                 reportTestFileErrors(
                     logger,
-                    args.browserNames,
-                    favoriteBrowser,
+                    browserNames,
+                    "Chromium",
                     failedSessions,
                     true
                 );
@@ -106,8 +119,6 @@ function customReporter(opts) {
     };
 }
 
-import defaultConfig from "./web-test-runner.config.mjs";
-
-defaultConfig.reporters = [customReporter()];
+defaultConfig.reporters = [ciReporter()];
 
 export default defaultConfig;
