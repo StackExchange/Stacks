fb81bc0b192e32bb34e6846b3e641400573071ae
Dan Cormier <dcormier@stackoverflow.com>
2023-04-25 17:03:18 -0400

    simplify template take 2

--- lib/components/block-link/block-link.visual.test.ts
+++ lib/components/block-link/block-link.visual.test.ts
@@ -2,13 +2,10 @@ import { html } from "@open-wc/testing";
 import { defaultOptions, runComponentTests } from "../../test/test-utils";
 import "../../index";
 
-const blockLinkTemplate = ({ component, testid, testidModifier }: {
-    component: unknown,
-    testid: string,
-    testidModifier?: string,
-}) => html`<div
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const blockLinkTemplate = ({ component, testid }: any) => html`<div
     class="d-inline-flex ai-center jc-center hs1 ws2 p8"
-    data-testid="${testid}${testidModifier ? `-${testidModifier}` : ""}"
+    data-testid="${testid}"
 >
     ${component}
 </div>`
