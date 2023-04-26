94428c80f07a75cddf9cff85ed3a223aa22d4f63
Dan Cormier <dcormier@stackoverflow.com>
2023-04-26 19:03:34 -0400

    test(button-group): add a11y, visual tests

---
+++ lib/components/button-group/button-group.a11y.test.ts
@@ -0,0 +1,61 @@
+import { defaultOptions, runComponentTests } from "../../test/test-utils";
+import "../../index";
+
+const getButton = ({
+    children = "",
+    isRadio,
+    isSelected,
+}: {
+    children: string;
+    isRadio?: boolean;
+    isSelected?: boolean;
+}): string => {
+    const baseClasses = "s-btn s-btn__muted s-btn__outlined";
+
+    return isRadio
+        ? `<input
+            class="s-btn--radio"
+            type="radio"
+            name="test-btn-radio-group"
+            id="btn-${children}"
+            ${isSelected ? "checked" : ""}/>
+            <label class="${baseClasses}" for="btn-${children}">
+                ${children}
+            </label>`
+        : `<button
+            class="${baseClasses}${isSelected ? " is-selected" : ""}"
+            ${isSelected ? `aria-current="true"` : ""}
+            role="button">
+                ${children}
+            </button>`
+};
+
+const getBaseButtons = (isRadio?: boolean): string => [
+    {children: "Newest", isRadio},
+    {children: "Frequent", isRadio},
+    {children: "Active", isSelected: true, isRadio},
+    {children: "Bountied", isRadio},
+    {children: "Unanswered", isSelected: !isRadio, isRadio},
+].map(getButton).join("");
+
+describe("button-group", () => {
+    [false, true].forEach((isRadio) => {
+        runComponentTests({
+            type: "a11y",
+            baseClass: "s-btn-group",
+            modifiers: isRadio
+                ? {primary: ["radio"] }
+                : {},
+            children: {
+                default: getBaseButtons(isRadio),
+            },
+            options: {
+                ...defaultOptions,
+                includeNullModifier: !isRadio,
+            },
+            // skippedTestids: [
+            //     "s-btn-group-dark",
+            // ],
+        });
+    });
+});
---
+++ lib/components/button-group/button-group.visual.test.ts
@@ -0,0 +1,68 @@
+import { html } from "@open-wc/testing";
+import { defaultOptions, runComponentTests } from "../../test/test-utils";
+import "../../index";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const buttonGroupTemplate = ({ component, testid }: any) => html`<div
+    class="bg-black-100 d-inline-flex ai-center jc-center p8"
+    data-testid="${testid}"
+>
+    ${component}
+</div>`;
+
+const getButton = ({
+    children = "",
+    isRadio,
+    isSelected,
+}: {
+    children: string;
+    isRadio?: boolean;
+    isSelected?: boolean;
+}): string => {
+    const baseClasses = "s-btn s-btn__muted s-btn__outlined";
+
+    return isRadio
+        ? `<input
+            class="s-btn--radio"
+            type="radio"
+            name="test-btn-radio-group"
+            id="btn-${children}"
+            ${isSelected ? "checked" : ""}/>
+            <label class="${baseClasses}" for="btn-${children}">
+                ${children}
+            </label>`
+        : `<button
+            class="${baseClasses}${isSelected ? " is-selected" : ""}"
+            ${isSelected ? `aria-current="true"` : ""}
+            role="button">
+                ${children}
+            </button>`
+};
+
+const getBaseButtons = (isRadio?: boolean): string => [
+    {children: "Newest", isSelected: true, isRadio},
+    {children: "Frequent", isRadio},
+    {children: "Active", isRadio},
+    {children: "Bountied", isRadio},
+    {children: "Unanswered", isRadio},
+].map(getButton).join("");
+
+describe("button-group", () => {
+    [false, true].forEach((isRadio) => {
+        runComponentTests({
+            type: "visual",
+            baseClass: "s-btn-group",
+            modifiers: isRadio
+                ? {primary: ["radio"] }
+                : {},
+            children: {
+                default: getBaseButtons(isRadio),
+            },
+            options: {
+                ...defaultOptions,
+                includeNullModifier: !isRadio,
+            },
+            template: buttonGroupTemplate,
+        });
+    });
+});
