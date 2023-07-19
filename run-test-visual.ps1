ecf45b2b1650ae546b93f12b03d5fb0796fc499f
Giamir Buoncristiani <giamir.buoncristiani@gmail.com>
2023-07-19 16:03:24 +0200

    switch to powershell script do support windows devices too

---
+++ run-test-visual.ps1
@@ -0,0 +1,20 @@
+$current_dir = if ($env:OS -eq "Windows_NT") { Get-Location } else { pwd }
+
+$dockerfile_path = Join-Path $current_dir "Dockerfile"
+@"
+FROM mcr.microsoft.com/playwright:v1.36.0-jammy
+WORKDIR /app
+COPY package*.json ./
+RUN npm ci
+"@ | Set-Content $dockerfile_path
+
+$image_hash = docker build -q -f $dockerfile_path .
+Remove-Item $dockerfile_path
+
+docker run --tty --rm -p 8000:8000 `
+  -v "${current_dir}/lib:/app/lib" `
+  -v "${current_dir}/screenshots:/app/screenshots" `
+  -v "${current_dir}/web-test-runner.config.mjs:/app/web-test-runner.config.mjs" `
+  -v "${current_dir}/web-test-runner.config.ci.mjs:/app/web-test-runner.config.ci.mjs" `
+  -v "${current_dir}/web-dev-server-patches.mjs:/app/web-dev-server-patches.mjs" `
+  $image_hash @args
