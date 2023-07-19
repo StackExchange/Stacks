477769cd9f0f4107d0a375c6dffed0fb5e5105bf
Giamir Buoncristiani <giamir.buoncristiani@gmail.com>
2023-07-19 11:42:16 +0200

    test(visual-regression): run on a docker container

---
+++ run-test-visual.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+current_dir=$(if [ "$(uname)" == "Darwin" ]; then echo $(pwd); else echo ${PWD}; fi)
+
+image_hash=$(docker build -q -f- . <<EOF
+FROM mcr.microsoft.com/playwright:v1.36.0-jammy
+WORKDIR /app
+COPY package*.json ./
+RUN npm ci
+EOF
+)
+
+docker run -it --rm -p 8000:8000 \
+  -v $current_dir/lib:/app/lib \
+  -v $current_dir/screenshots:/app/screenshots \
+  -v $current_dir/web-test-runner.config.mjs:/app/web-test-runner.config.mjs \
+  -v $current_dir/web-dev-server-patches.mjs:/app/web-dev-server-patches.mjs \
+  $image_hash "$@"
