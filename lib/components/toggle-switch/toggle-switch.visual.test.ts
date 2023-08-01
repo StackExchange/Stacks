ddcd5d50fe92558fcbe65fc3b86ea3a7389453f3
Dan Cormier <dcormier@stackoverflow.com>
2023-08-01 17:03:59 -0400

    fix(toggle-switch): apply lower opacity when disabled (#1402)
    * fix(toggle-switch): apply lower opacity when disabled
    
    * add visual and a11y tests
    
    * lint!
    
    * Revert "lint!"
    
    This reverts commit 28a56b575adce24912f0428eebae73a7f7df395c.
    
    * Revert "add visual and a11y tests"
    
    This reverts commit b467f723d494bc8a75c93cc34a676ce1a1e6f3ab.
    
    * Add visual and a11y tests (again)
    
    * Add baseline images for toggle switch
    
    * Trying to make the tests pass…

---
+++ lib/components/toggle-switch/toggle-switch.visual.test.ts
@@ -0,0 +1,73 @@
+import { html } from "@open-wc/testing";
+import { defaultOptions, runComponentTests } from "../../test/test-utils";
+import "../../index";
+
+describe("toggle-switch", () => {
+    // Single toggle switch
+    [true, false].forEach((checked) => {
+        [true, false].forEach((disabled) => {
+            const idSuffix = `${checked ? "-checked" : ""}${
+                disabled ? "-disabled" : ""
+            }`;
+            const id = `toggle-switch${idSuffix}`;
+
+            runComponentTests({
+                type: "visual",
+                baseClass: "s-toggle-switch",
+                modifiers: {
+                    global: idSuffix ? [idSuffix.substring(1)] : [], // for proper testid generation
+                },
+                tag: "input",
+                attributes: {
+                    id,
+                    type: "checkbox",
+                    ...(checked ? { checked: "" } : {}),
+                    ...(disabled ? { disabled: "" } : {}),
+                },
+                template: ({ component, testid }) => html`
+                    <div data-testid="${testid}" class="p4 ws1">
+                        <label class="v-visible-sr" for="${id}">toggle</label>
+                        ${component}
+                    </div>
+                `,
+            });
+        });
+    });
+
+    // Multiple toggle switch variant
+    [true, false].forEach((offChecked) => {
+        runComponentTests({
+            type: "visual",
+            baseClass: "s-toggle-switch",
+            variants: ["multiple"],
+            modifiers: {
+                global: offChecked ? ["off"] : [], // for proper testid generation
+            },
+            children: {
+                default: `
+                    <input type="radio" name="group" id="four" ${
+                        offChecked ? 'checked=""' : ""
+                    }>
+                    <label for="four" class="s-toggle-switch--label-off">Off</label>
+                    <input type="radio" name="group" id="one" ${
+                        !offChecked ? 'checked=""' : ""
+                    }>
+                    <label for="one">Weekly</label>
+                    <input type="radio" name="group" id="two">
+                    <label for="two">Daily</label>
+                    <input type="radio" name="group" id="three">
+                    <label for="three">3 hrs</label>
+                `,
+            },
+            options: {
+                ...defaultOptions,
+                includeNullVariant: false,
+            },
+            template: ({ component, testid }) => html`
+                <div data-testid="${testid}" class="d-flex ai-center g8 p4 ws2">
+                    ${component}
+                </div>
+            `,
+        });
+    });
+});
