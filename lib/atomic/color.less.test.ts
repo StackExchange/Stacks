cd947427687bbd7c4b513639914f0e4f91d7207a
Dan Cormier <dcormier@stackoverflow.com>
2023-08-10 14:08:19 -0400

    Update less tests to import from absolute paths
    lib/exports/color.less.test.ts was importing "color.less", which would import "lib/atomic.color.less" for some reason. I changed the paths to be explicit and work no matter what directory we chuck this files into.

--- lib/atomic/color.less.test.ts
+++ lib/atomic/color.less.test.ts
@@ -4,7 +4,7 @@ import { renderLess } from "../test/less-test-utils";
 describe("atomic: color", () => {
     it("should output all atomic css classes for colors", async () => {
         const css = await renderLess(`
-            @import "color.less";
+            @import "./lib/atomic/color.less";
         `);
 
         expect(css).toMatchSnapshot();
--- lib/atomic/v1/color.less.test.ts
+++ lib/atomic/v1/color.less.test.ts
@@ -4,9 +4,9 @@ import { renderLess } from "../../test/less-test-utils";
 describe("atomic: color v1", () => {
     it("should output all old atomic css classes for colors", async () => {
         const css = await renderLess(`
-            @import (reference) "v1/constants-colors.less";
-            @import (reference) "../mixins.less";
-            @import "color.less";
+            @import (reference) "./lib/exports/v1/constants-colors.less";
+            @import (reference) "./lib/exports/mixins.less";
+            @import "./lib/atomic/v1/color.less";
         `);
 
         expect(css).toMatchSnapshot();
