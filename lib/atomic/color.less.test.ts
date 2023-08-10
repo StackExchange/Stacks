0f3ca4b13390fe81cd9a5bfe4da5dc7d1f9932ff
Dan Cormier <dcormier@stackoverflow.com>
2023-08-10 14:28:12 -0400

    Standardize around v1/v2 for legacy/new colors

--- lib/atomic/color.less.test.ts
+++ lib/atomic/color.less.test.ts
@@ -2,7 +2,7 @@ import { describe, it, expect } from "vitest";
 import { renderLess } from "../test/less-test-utils";
 
 describe("atomic: color", () => {
-    it("should output all atomic css classes for colors", async () => {
+    it("should output v2 atomic css classes for colors", async () => {
         const css = await renderLess(`
             @import "./lib/atomic/color.less";
         `);
