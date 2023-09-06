2856df1ecbbb05b8582910aae83bf8377c515567
Dan Cormier <dcormier@stackoverflow.com>
2023-09-06 17:43:43 -0400

    test(navigation): add a11y; visual tests

---
+++ lib/components/navigation/navigation.a11y.test.ts
@@ -0,0 +1,61 @@
+import { html } from "@open-wc/testing";
+import { runComponentTests } from "../../test/test-utils";
+import "../../index";
+
+const items = [
+    {
+        label: "Group 1",
+        title: true
+    }, {
+        label: "Product",
+        selected: true
+    }, {
+        label: "Email",
+    }, {
+        label: "Group 2",
+        title: true
+    }, {
+        label: "Content"
+    }, {
+        label: "Brand"
+    }, {
+        label: "Marketing"
+    }, {
+        label: "More selected",
+        dropdown: true,
+        selected: true
+    }, {
+        label: "More",
+        dropdown: true,
+    }
+];
+
+const getChildren = (includeTitles = false): string => items.map(item => {
+    if (item.title) {
+        return includeTitles ? `<li class="s-navigation--title">${item.label}</li>` : "";
+    }
+    const classes = `s-navigation--item${item.selected ? " is-selected" : ""}${item.dropdown ? " s-navigation--item__dropdown" : ""}`;
+    return `<li><a href="#" class="${classes}">${item.label}</a></li>`;
+}).join("");
+
+describe("navigation", () => {
+    runComponentTests({
+        type: "a11y",
+        baseClass: "s-navigation",
+        variants: ["vertical", "muted"],
+        modifiers: {
+            primary: ["scroll", "sm"],
+        },
+        tag: "ul",
+        children: {
+            default: getChildren(true),
+        },
+        template: ({ component, testid }) => html`
+            <nav class="d-inline-block p8 wmx3" aria-label="example-navigation" data-testid="${testid}">
+                ${component}
+            </nav>
+        `,
+        // TODO new colors should resolve the `.is-selected` failures
+        skippedTestids: [/s-navigation/],
+    });
+});
---
+++ lib/components/navigation/navigation.visual.test.ts
@@ -0,0 +1,78 @@
+import { html } from "@open-wc/testing";
+import { defaultOptions, runComponentTests } from "../../test/test-utils";
+import "../../index";
+
+const items = [
+    {
+        label: "Group 1",
+        title: true
+    }, {
+        label: "Product",
+        selected: true
+    }, {
+        label: "Email",
+    }, {
+        label: "Group 2",
+        title: true
+    }, {
+        label: "Content"
+    }, {
+        label: "Brand"
+    }, {
+        label: "Marketing"
+    }, {
+        label: "More selected",
+        dropdown: true,
+        selected: true
+    }, {
+        label: "More",
+        dropdown: true,
+    }
+];
+
+const getChildren = (includeTitles = false): string => items.map(item => {
+    if (item.title) {
+        return includeTitles ? `<li class="s-navigation--title">${item.label}</li>` : "";
+    }
+    const classes = `s-navigation--item${item.selected ? " is-selected" : ""}${item.dropdown ? " s-navigation--item__dropdown" : ""}`;
+    return `<li><a href="#" class="${classes}">${item.label}</a></li>`;
+}).join("");
+
+describe("navigation", () => {
+    runComponentTests({
+        type: "visual",
+        baseClass: "s-navigation",
+        variants: ["muted"],
+        modifiers: {
+            primary: ["scroll", "sm"],
+        },
+        tag: "ul",
+        children: {
+            default: getChildren(),
+        },
+        template: ({ component, testid }) => html`
+            <nav class="d-inline-block p8 wmx3" aria-label="example-navigation" data-testid="${testid}">
+                ${component}
+            </nav>
+        `,
+    });
+
+    runComponentTests({
+        type: "visual",
+        baseClass: "s-navigation",
+        variants: ["vertical"],
+        tag: "ul",
+        children: {
+            default: getChildren(true),
+        },
+        template: ({ component, testid }) => html`
+            <nav class="d-inline-block p8 ws2" aria-label="example-navigation" data-testid="${testid}">
+                ${component}
+            </nav>
+        `,
+        options: {
+            ...defaultOptions,
+            includeNullVariant: false,
+        },
+    });
+});
