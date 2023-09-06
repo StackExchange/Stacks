2196271c85af91757962ef4bd8f3ab0667654235
Dan Cormier <dcormier@stackoverflow.com>
2023-09-06 19:03:17 -0400

    formatting!

--- lib/components/navigation/navigation.a11y.test.ts
+++ lib/components/navigation/navigation.a11y.test.ts
@@ -5,38 +5,53 @@ import "../../index";
 const items = [
     {
         label: "Group 1",
-        title: true
-    }, {
+        title: true,
+    },
+    {
         label: "Product",
-        selected: true
-    }, {
+        selected: true,
+    },
+    {
         label: "Email",
-    }, {
+    },
+    {
         label: "Group 2",
-        title: true
-    }, {
-        label: "Content"
-    }, {
-        label: "Brand"
-    }, {
-        label: "Marketing"
-    }, {
+        title: true,
+    },
+    {
+        label: "Content",
+    },
+    {
+        label: "Brand",
+    },
+    {
+        label: "Marketing",
+    },
+    {
         label: "More selected",
         dropdown: true,
-        selected: true
-    }, {
+        selected: true,
+    },
+    {
         label: "More",
         dropdown: true,
-    }
+    },
 ];
 
-const getChildren = (includeTitles = false): string => items.map(item => {
-    if (item.title) {
-        return includeTitles ? `<li class="s-navigation--title">${item.label}</li>` : "";
-    }
-    const classes = `s-navigation--item${item.selected ? " is-selected" : ""}${item.dropdown ? " s-navigation--item__dropdown" : ""}`;
-    return `<li><a href="#" class="${classes}">${item.label}</a></li>`;
-}).join("");
+const getChildren = (includeTitles = false): string =>
+    items
+        .map((item) => {
+            if (item.title) {
+                return includeTitles
+                    ? `<li class="s-navigation--title">${item.label}</li>`
+                    : "";
+            }
+            const classes = `s-navigation--item${
+                item.selected ? " is-selected" : ""
+            }${item.dropdown ? " s-navigation--item__dropdown" : ""}`;
+            return `<li><a href="#" class="${classes}">${item.label}</a></li>`;
+        })
+        .join("");
 
 describe("navigation", () => {
     runComponentTests({
@@ -51,7 +66,11 @@ describe("navigation", () => {
             default: getChildren(true),
         },
         template: ({ component, testid }) => html`
-            <nav class="d-inline-block p8 wmx3" aria-label="example-navigation" data-testid="${testid}">
+            <nav
+                class="d-inline-block p8 wmx3"
+                aria-label="example-navigation"
+                data-testid="${testid}"
+            >
                 ${component}
             </nav>
         `,
