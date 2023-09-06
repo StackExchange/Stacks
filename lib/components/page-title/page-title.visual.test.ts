601b2e13575f2c4010cbc7034d334f7ccc1a9c52
Dan Cormier <dcormier@stackoverflow.com>
2023-09-06 18:59:52 -0400

    test(page-title): add a11y; visual tests

---
+++ lib/components/page-title/page-title.visual.test.ts
@@ -0,0 +1,53 @@
+import { html } from "@open-wc/testing";
+import { defaultOptions, runComponentTests } from "../../test/test-utils";
+import "../../index";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const postTitleTemplate  = ({ component, testid }: any) => html`
+<div class="d-block p8 ws6" data-testid="${testid}">
+    ${component}
+</div>
+`;
+
+const getChildren = ({ showEverything }: { showEverything: boolean }) => `
+    <div class="s-page-title--text">
+        ${showEverything ? `
+            <nav class="s-breadcrumbs" aria-label="example-breadcrumbs">
+                <div class="s-breadcrumbs--item">
+                    <a class="s-breadcrumbs--link">Stack Overflow</a>
+                </div>
+            </nav>
+        ` : ""}
+        <h1 class="s-page-title--header">Page title</h1>
+        ${showEverything ? `
+            <p class="s-page-title--description">
+                Optional description de Finibus Bonorum et Malorum
+            </p>
+        ` : ""}
+    </div>
+    ${showEverything ? `
+        <div class="s-page-title--actions">
+            <button class="s-btn s-btn__filled" type="button">Action</button>
+        </div>
+    ` : ""}
+`;
+
+describe("page title", () => {
+    [true, false].map((showEverything) => {
+        runComponentTests({
+            type: "visual",
+            baseClass: "s-page-title",
+            modifiers: showEverything ? {
+                global: ["complete"],
+            } : {},
+            children: {
+                default: getChildren({ showEverything }),
+            },
+            template: postTitleTemplate,
+            options: showEverything ? {
+                ...defaultOptions,
+                includeNullModifier: false,
+            } : defaultOptions,
+        });
+    });
+});
