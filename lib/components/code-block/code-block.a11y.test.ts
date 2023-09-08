372b69415ac54363d851a2a77a5bff4cbfce1ae5
Dan Cormier <dcormier@stackoverflow.com>
2023-09-08 17:19:57 -0400

    test(code-block): add a11y; visual tests

---
+++ lib/components/code-block/code-block.a11y.test.ts
@@ -0,0 +1,34 @@
+import { defaultOptions, runComponentTests } from "../../test/test-utils";
+import testHTML from "./code-block-test-html";
+import type { Language } from "./code-block-test-html";
+import "../../index";
+
+// TODO should we test this many languages? More? Fewer?
+const languages: Language[] = ["css", "html", "javascript"];
+
+describe("code block", () => {
+    languages.forEach((language: Language) => {
+        runComponentTests({
+            type: "a11y",
+            tag: "pre",
+            baseClass: `s-code-block language-${language}`,
+            children: {
+                default: testHTML[language],
+            },
+            options: {
+                ...defaultOptions,
+                includeNullModifier: false,
+            },
+            // TODO new colors should resolve this issue
+            skippedTestids: [
+                "s-code-block-language-html-light",
+                "s-code-block-language-html-highcontrast-light",
+                "s-code-block-language-html-highcontrast-dark",
+                "s-code-block-language-html-dark",
+                "s-code-block-language-css-dark",
+                "s-code-block-language-javascript-dark",
+                "s-code-block-language-javascript-highcontrast-light",
+            ]
+        });
+    });
+});
---
+++ lib/components/code-block/code-block.visual.test.ts
@@ -0,0 +1,24 @@
+import { defaultOptions, runComponentTests } from "../../test/test-utils";
+import testHTML from "./code-block-test-html";
+import type { Language } from "./code-block-test-html";
+import "../../index";
+
+// TODO should we test this many languages? More? Fewer?
+const languages: Language[] = ["css", "html", "javascript"];
+
+describe("code block", () => {
+    languages.forEach((language: Language) => {
+        runComponentTests({
+            type: "visual",
+            tag: "pre",
+            baseClass: `s-code-block language-${language}`,
+            children: {
+                default: testHTML[language],
+            },
+            options: {
+                ...defaultOptions,
+                includeNullModifier: false,
+            }
+        });
+    });
+});
