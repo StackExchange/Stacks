24fbe3dc574b7ed0afa2cbc8a08ea2412b370255
Dan Cormier <dcormier@stackoverflow.com>
2023-09-15 16:38:58 -0400

    test(tag): add a11y; visual tests

---
+++ lib/components/tag/tag.visual.test.ts
@@ -0,0 +1,48 @@
+import { html } from "@open-wc/testing";
+import { IconClearSm } from "@stackoverflow/stacks-icons";
+import { defaultOptions, runComponentTests } from "../../test/test-utils";
+import "../../index";
+
+const children = {
+    default: `default`,
+    dismiss: `dismiss <span class="s-tag--dismiss">${IconClearSm}</span>`,
+    sponsor: `<img class="s-tag--sponsor" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAIAQMAAAD+wSzIAAAABlBMVEX///+/v7+jQ3Y5AAAADklEQVQI12P4AIX8EAgALgAD/aNpbtEAAAAASUVORK5CYII" width="16" height="16" alt="sponsor">sponsor`,
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const template = ({ component, testid }: any) => html`
+    <div data-testid="${testid}" class="d-inline-block p4">
+        ${component}
+    </div>
+`;
+
+describe("tag", () => {
+    runComponentTests({
+        type: "visual",
+        baseClass: "s-tag",
+        variants: ["ignored", "watched", "moderator", "muted", "required"],
+        modifiers: {
+            global: ["is-selected"],
+        },
+        children,
+        template,
+    });
+
+    // Size modifiers
+    runComponentTests({
+        type: "visual",
+        baseClass: "s-tag",
+        modifiers: {
+            primary: ["xs", "sm", "md", "lg"],
+            global: ["is-selected"],
+        },
+        children: {
+            default: `default`,
+        },
+        template,
+        options: {
+            ...defaultOptions,
+            includeNullModifier: false,
+        }
+    });
+});
