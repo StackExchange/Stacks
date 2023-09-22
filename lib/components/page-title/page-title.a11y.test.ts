15eda86647937e27df7b75f40c914377012a441f
Dan Cormier <dcormier@stackoverflow.com>
2023-09-22 10:22:59 -0400

    test(page-title): add a11y; visual tests (#1480)
    * test(page-title): add a11y; visual tests
    
    * Formatting!
    
    * Update page-title.a11y.test.ts
    
    * Update page-title.a11y.test.ts
    
    * Update page-title.visual.test.ts
    
    * Formatting
    
    * Update page-title.visual.test.ts

---
+++ lib/components/page-title/page-title.a11y.test.ts
@@ -0,0 +1,31 @@
+import { html } from "@open-wc/testing";
+import { runComponentTests } from "../../test/test-utils";
+import "../../index";
+
+describe("page title", () => {
+    runComponentTests({
+        type: "a11y",
+        baseClass: "s-page-title",
+        children: {
+            default: `
+                <div class="s-page-title--text">
+                    <nav class="s-breadcrumbs" aria-label="example-breadcrumbs">…</nav>
+                    <h1 class="s-page-title--header">Page title</h1>
+                    <p class="s-page-title--description">
+                        Optional description de Finibus Bonorum et Malorum
+                    </p>
+                </div>
+                <div class="s-page-title--actions">
+                    <button class="s-btn s-btn__filled" type="button">Action</button>
+                </div>
+            `,
+        },
+        template: ({ component, testid }) => html`
+            <div class="d-block p8 ws6" data-testid="${testid}">
+                ${component}
+            </div>
+        `,
+        // TODO new colors should resolve this issue
+        skippedTestids: [/s-page-title-dark/, /s-page-title-light/],
+    });
+});
