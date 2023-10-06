273cdcc3a730f2bf9f18fa1d003b70c4e8c96180
Dan Cormier <dcormier@stackoverflow.com>
2023-10-06 14:08:03 -0400

    tests(input-fill): add a11y; visual tests

---
+++ lib/components/input-fill/input-fill.a11y.test.ts
@@ -0,0 +1,24 @@
+import { html } from "@open-wc/testing";
+import { runComponentTests } from "../../test/test-utils";
+import "../../index";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const template = ({ component, testid }: any) => html`
+    <div data-testid="${testid}" class="p8 ws1">
+        ${component}
+    </div>
+`;
+
+describe("input-fill", () => {
+    runComponentTests({
+        type: "a11y",
+        baseClass: `s-input-fill`,
+        modifiers: {
+            global: ["order-first", "order-last"],
+        },
+        children: {
+            default: "input fill",
+        },
+        template,
+    });
+});
