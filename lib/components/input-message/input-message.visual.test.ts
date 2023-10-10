6f84729b1ba5f9211334d306ff691bbe80eff7e0
Dan Cormier <dcormier@stackoverflow.com>
2023-10-10 11:42:14 -0400

    test(input-message): add a11y; visual tests

---
+++ lib/components/input-message/input-message.visual.test.ts
@@ -0,0 +1,40 @@
+import { html } from "@open-wc/testing";
+import { runComponentTests } from "../../test/test-utils";
+import "../../index";
+
+const states = [{
+    class: "",
+    children: 'This is a stateless input message. <a href="">Learn more</a>.',
+}, {
+    class: "has-error",
+    children: 'This is an error input message. <a href="">Learn more</a>.',
+}, {
+    class: "has-success",
+    children: 'This is a success input message. <a href="">Learn more</a>.',
+}, {
+    class: "has-warning",
+    children: 'This is a warning input message. <a href="">Learn more</a>.',
+}, {
+    class: "disabled",
+    children: 'This is a disabled input message. <a href="">Learn more</a>.',
+}];
+
+describe("input-message", () => {
+    states.forEach((state) => {
+        runComponentTests({
+            type: "visual",
+            baseClass: `s-input-message ${state.class ? `state-${state.class}` : ""}`,
+            children: {
+                default: state.children,
+            },
+            template: ({ component, testid }) => {
+                const isDisabled = state.class === "disabled";
+                const stateClass = state.class && state.class !== "disabled" ? state.class : "";
+
+                return  html`
+                    <fieldset data-testid="${testid}" class="p8 ws3 ${stateClass}" ?disabled="${isDisabled}">${component}</fieldset>
+                `;
+            },
+        });
+    });
+});
