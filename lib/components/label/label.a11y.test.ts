14725d2a0baf298bd93366fb1cfd2a565e671808
Dan Cormier <dcormier@stackoverflow.com>
2023-10-18 17:40:54 -0400

    test(label): add a11y; visual tests

---
+++ lib/components/label/label.a11y.test.ts
@@ -0,0 +1,48 @@
+import { html } from "@open-wc/testing";
+import { runComponentTests } from "../../test/test-utils";
+import "../../index";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const labelTemplate = ({ component, testid }: any) => {
+    return html`
+        <fieldset data-testid="${testid}" class="p8 ws3">${component}</fieldset>
+    `;
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const getChildren = (status?: any) => {
+    const typeClass =
+        status && status !== "base" ? `s-label--status__${status}` : "";
+    return `
+        Example label
+        ${
+            status
+                ? `
+            <span class="s-label--status ${typeClass}">${
+                status ?? "no type"
+            }</span>
+        `
+                : ""
+        }
+    `;
+};
+
+describe("label", () => {
+    runComponentTests({
+        type: "a11y",
+        baseClass: `s-label`,
+        modifiers: {
+            primary: ["sm", "md", "lg", "xl"],
+        },
+        children: {
+            "default": getChildren(),
+            "status": getChildren("base"),
+            "status-beta": getChildren("beta"),
+            "status-new": getChildren("new"),
+            "status-required": getChildren("required"),
+        },
+        tag: "label",
+        template: ({ component, testid }) =>
+            labelTemplate({ component, testid }),
+    });
+});
