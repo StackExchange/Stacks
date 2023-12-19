e32b22f93e5708769f01d640f768996f5a590773
Dan Cormier <dcormier@stackoverflow.com>
2023-12-19 17:27:08 -0500

    Implement tests

--- lib/components/post-summary/post-summary.a11y.test.ts
+++ lib/components/post-summary/post-summary.a11y.test.ts
@@ -1,41 +1,171 @@
+import { html } from "@open-wc/testing";
 import { runComponentTests } from "../../test/test-utils";
+import { getChildren } from "../../test/post-summary-test-utils";
+import type { BadgeType, TruncationSizes } from "../../test/post-summary-test-utils";
 import "../../index";
 
-describe("post summary", () => {
-    // States
-        // default, watched, ignored, deleted
-    // modifiers
-        // minimal
-    // child elements
-        // answer
-        // answer excerpt
-        // content
-        // content excerpt
-            // default, sm, md, lg
-        // content menu button
-        // content title
-        // content type
-        // meta
-            // user card
-        // meta tags
-            // tag
-        // stats
-        // stats item
-            // votes
-            // answers
-                // default, answered, accepted
-            // views
-                // default, warm, hot, supernova
-            // bounty
-        // stats item number
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const template = ({ component, testid }: any) => html`
+    <div class="d-flex ai-center jc-center p8 ws2" data-testid="${testid}">
+        ${component}
+    </div>
+`;
 
-    // default, sizes
+describe("post-summary", () => {
+    // Base, sparce
     runComponentTests({
-        type: "visual",
+        type: "a11y",
         baseClass: "s-post-summary",
-        // children: {
-        //     default: child(),
-        // },
-        // template,
+        modifiers: {
+            primary: ["deleted", "ignored", "watched"], // variants described as modifiers to test colliding modifiers
+            secondary: ["minimal"],
+        },
+        children: {
+            default: getChildren({
+                show: {
+                    description: true,
+                    menuBtn: true,
+                    stats: true,
+                    tags: true,
+                    title: true,
+                    user: true,
+                }
+            }),
+            sparce: getChildren({
+                show: {
+                    stats: true,
+                    tags: true,
+                    title: true,
+                    user: true,
+                },
+                tags: [{
+                    text: "featured-request",
+                    type: "required",
+                }],
+                title: "Short title",
+            }),
+        },
+        template,
+        // TODO resolve test failures
+        skippedTestids: [
+            /-deleted/,
+            /-ignored/,
+            /-highcontrast-(light|dark)-watched/,
+        ]
+    });
+
+    // Truncated description sizes
+    ["sm", "md", "lg"].forEach((size) => {
+        runComponentTests({
+            type: "a11y",
+            baseClass: "s-post-summary",
+            children: {
+                [`description-${size}`]: getChildren({
+                    show: {
+                        description: true,
+                        menuBtn: true,
+                        stats: true,
+                        tags: true,
+                        title: true,
+                        user: true,
+                    },
+                    description: {
+                        truncation: size as TruncationSizes,
+                    },
+                }),
+            },
+            template,
+        });
+    });
+
+    // Stats - answers
+    [
+        {
+            childName: "stats-unanswered",
+            answers: 0,
+        }, {
+            childName: "stats-answered",
+            answers: 1,
+        }, {
+            childName: "stats-answered-accepted",
+            answers: 10,
+            accepted: true,
+        }
+    ].forEach(({ childName, answers, accepted }) => {
+        runComponentTests({
+            type: "a11y",
+            baseClass: "s-post-summary",
+            children: {
+                [childName]: getChildren({
+                    show: {
+                        stats: true,
+                    },
+                    stats: {
+                        answers,
+                        votes: 1,
+                        views: 20,
+                        accepted,
+                    },
+                }),
+            },
+            template,
+        });
+    });
+
+    // Stats - view hotness
+    [
+        {
+            childName: "stats-views",
+            views: 1,
+        }, {
+            childName: "stats-views-warm",
+            views: 1001,
+        }, {
+            childName: "stats-views-hot",
+            views: 10001,
+        }, {
+            childName: "stats-views-supernova",
+            views: 100001,
+        }
+    ].forEach(({ childName, views }) => {
+        runComponentTests({
+            type: "a11y",
+            baseClass: "s-post-summary",
+            children: {
+                [childName]: getChildren({
+                    show: {
+                        stats: true,
+                    },
+                    stats: {
+                        answers: 1,
+                        votes: 1,
+                        views,
+                    },
+                }),
+            },
+            template,
+        });
+    });
+
+    // Badges
+    ["danger", "danger-filled", "info", "muted", "muted-filled", "warning"].forEach((badge) => {
+        runComponentTests({
+            type: "a11y",
+            baseClass: "s-post-summary",
+            children: {
+                [`badge-${badge}`]: getChildren({
+                    show: {
+                        stats: true,
+                    },
+                    stats: {
+                        badge: badge as BadgeType,
+                        answers: 0,
+                        votes: 1,
+                        views: 20,
+                    },
+                })
+            },
+            template,
+        });
     });
 });
