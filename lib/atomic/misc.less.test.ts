8a632ef52d7378794e4dc471b9a26f1abeed1de2
Dan Cormier <dcormier@stackoverflow.com>
2024-02-07 10:30:50 -0500

    That's not v1…

--- lib/atomic/misc.less.test.ts
+++ lib/atomic/misc.less.test.ts
@@ -1,7 +1,7 @@
 import { describe, it, expect } from "vitest";
 import { renderLess } from "../test/less-test-utils";
 
-describe("atomic: misc v1", () => {
+describe("atomic: misc", () => {
     it("should output all atomic css classes", async () => {
         const css = await renderLess(`
             @import (reference) "./lib/base/internal.less";
