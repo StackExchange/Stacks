72d167105306fff235748493854f605b76237a2a
Dan Cormier <dcormier@stackoverflow.com>
2024-02-29 12:53:41 -0500

    Move duplicated test code to setup file
    As suggested in https://github.com/StackExchange/Stacks/pull/1346#pullrequestreview-1867360975

--- lib/components/button-group/button-group.a11y.test.ts
+++ lib/components/button-group/button-group.a11y.test.ts
@@ -1,51 +1,7 @@
+import { testArgs } from "./button-group.test.setup";
 import { runA11yTests } from "../../test/a11y-test-utils";
 import "../../index";
 
-const btns = [
-    { name: "Newest", isSelected: true },
-    { name: "Frequent" },
-    { name: "Active" },
-    { name: "Bountied" },
-    { name: "Unanswered" },
-];
-
-const getBtn = ({
-    name = "",
-    isRadio,
-    isSelected,
-}: {
-    name: string;
-    isRadio?: boolean;
-    isSelected?: boolean;
-}): string => {
-    const baseClasses = "s-btn s-btn__muted";
-
-    return isRadio
-        ? `<input
-            class="s-btn--radio"
-            type="radio"
-            name="test-btn-radio-group"
-            id="btn-${name}"
-            ${isSelected ? "checked" : ""}/>
-            <label class="${baseClasses}" for="btn-${name}">
-                <span class="s-btn--text" data-text="${name}">${name}</span>
-            </label>`
-        : `<button
-            class="${baseClasses}${isSelected ? " is-selected" : ""}"
-            ${isSelected ? `aria-current="true"` : ""}
-            type="button">
-                <span class="s-btn--text" data-text="${name}">${name}</span>
-            </button>`;
-};
-
 describe("button group", () => {
-    runA11yTests({
-        baseClass: "s-btn-group",
-        children: {
-            default: btns.map((btn) => getBtn({ ...btn })).join(""),
-            radio: btns
-                .map((btn) => getBtn({ ...btn, isRadio: true }))
-                .join(""),
-        },
-    });
+    runA11yTests(testArgs);
 });
---
+++ lib/components/button-group/button-group.test.setup.ts
@@ -0,0 +1,69 @@
+import { html } from "@open-wc/testing";
+import type { TestVariationArgs } from "../../test/test-utils";
+
+const btns = [
+    { name: "Newest", isSelected: true },
+    { name: "Frequent" },
+    { name: "Active" },
+    { name: "Bountied" },
+    { name: "Unanswered" },
+];
+
+const getBtn = ({
+    name = "",
+    isRadio,
+    isSelected,
+}: {
+    name: string;
+    isRadio?: boolean;
+    isSelected?: boolean;
+}): string => {
+    const baseClasses = "s-btn s-btn__muted";
+
+    return isRadio
+        ? `<input
+            class="s-btn--radio"
+            type="radio"
+            name="test-btn-radio-group"
+            id="btn-${name}"
+            ${isSelected ? "checked" : ""}/>
+            <label class="${baseClasses}" for="btn-${name}">
+                <span class="s-btn--text" data-text="${name}">${name}</span>
+            </label>`
+        : `<button
+            class="${baseClasses}${isSelected ? " is-selected" : ""}"
+            ${isSelected ? `aria-current="true"` : ""}
+            type="button">
+                <span class="s-btn--text" data-text="${name}">${name}</span>
+            </button>`;
+};
+
+const getBtns = (ids: number[]): string => {
+    return ids.map((id) => getBtn(btns[id])).join("");
+};
+
+const testArgs: TestVariationArgs = {
+    baseClass: "s-btn-group",
+    children: {
+        "single": getBtns([0]),
+        "many": getBtns([0, 1, 2, 3]),
+        "many-with-form": `
+            ${getBtns([0])}
+            <form class="mb0 p0">
+                ${getBtn(btns[1])}
+            </form>
+            ${getBtns([2, 3])}
+        `,
+        "radio": btns.map((btn) => getBtn({ ...btn, isRadio: true })).join(""),
+    },
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    template: ({ component, testid }: any) =>
+        html`<div
+            class="d-inline-flex ai-center jc-center p8"
+            data-testid="${testid}"
+        >
+            ${component}
+        </div>`,
+};
+
+export { testArgs };
