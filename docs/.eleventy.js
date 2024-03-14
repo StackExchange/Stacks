bceabe7d4462976ec46466e6a2b8dd1e3ad73700
Dan Cormier <dcormier@stackoverflow.com>
2024-03-14 18:07:37 -0400

    Replace color-swatch plugin with include

--- docs/.eleventy.js
+++ docs/.eleventy.js
@@ -7,7 +7,6 @@ const iconPlugin = require("./plugins/icons");
 const bannerExamplePlugin = require("./plugins/banner-example");
 const tipPlugin = require("./plugins/tip");
 const markdownPlugin = require("./plugins/markdown");
-const colorSwatchPlugin = require("./plugins/color-swatch");
 const stacksMarkdownPlugin = require("./plugins/stacks-markdown");
 
 module.exports = function(eleventyConfig) {
@@ -19,7 +18,6 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(iconPlugin);
   eleventyConfig.addPlugin(headerPlugin);
   eleventyConfig.addPlugin(tipPlugin);
-  eleventyConfig.addPlugin(colorSwatchPlugin);
   eleventyConfig.addPlugin(stacksMarkdownPlugin);
 
   // Version shortcode
