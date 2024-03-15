7d45725aed0ad984581db92f12dff97bd12a981b
Dan Cormier <dcormier@stackoverflow.com>
2024-03-15 16:21:14 -0400

    Clean up generateHeaderHTML function

--- docs/plugins/header.js
+++ docs/plugins/header.js
@@ -1,21 +1,20 @@
 const { Icons } = require("@stackoverflow/stacks-icons");
 
-const generateHeaderHTML = function(tag, text) {
+const generateHeaderHTML = (tag, text) => {
   var slug = text.replace(/\s+/g, '-').toLowerCase();
   var linkIcon = Icons.IconLink;
 
-  var output = '';
-  output += '<div class="d-flex jc-space-between ai-end pe-none stacks-header">';
-  output +=   '<' + tag + ' class="flex--item fl-grow1 stacks-' + tag + '" id="'+ slug +'">';
-  output +=     '<span class="pe-auto">' + text + '</span>';
-  output +=   '</' + tag + '>';
-  output +=   '<a class="d-flex flex__center mbn6 s-btn s-btn__muted pe-auto" href="#'+ slug +'">';
-  output +=     '<span class="v-visible-sr">Section titled ' + text + '</span>';
-  output +=     linkIcon;
-  output +=   '</a>';
-  output += '</div>';
-
-  return output;
+  return `
+    <div class="d-flex jc-space-between ai-end pe-none stacks-header">
+      <${tag} class="flex--item fl-grow1 stacks-${tag}" id="${slug}">
+        <span class="pe-auto">${text}</span>
+      </${tag}>
+      <a class="d-flex flex__center mbn6 s-btn s-btn__muted pe-auto" href="#${slug}">
+        <span class="v-visible-sr">Section titled ${text}</span>
+        ${linkIcon}
+      </a>
+    </div>
+  `;
 };
 
 const header = {
