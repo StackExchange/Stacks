ba4a5ee9f706429872e7eac3c42010fa79f67a0f
Giamir Buoncristiani <giamir.buoncristiani@gmail.com>
2024-05-03 10:48:28 +0200

    add visual and a11y tests for required symbol

--- lib/components/label/label.a11y.test.ts
+++ lib/components/label/label.a11y.test.ts
@@ -1,48 +1,6 @@
-import { html } from "@open-wc/testing";
 import { runA11yTests } from "../../test/a11y-test-utils";
-import "../../index";
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const labelTemplate = ({ component, testid }: any) => {
-    return html`
-        <fieldset data-testid="${testid}" class="p8 ws3">${component}</fieldset>
-    `;
-};
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const getChildren = (status?: any) => {
-    const typeClass =
-        status && status !== "base" ? `s-label--status__${status}` : "";
-    return `
-        Example label
-        ${
-            status
-                ? `
-            <span class="s-label--status ${typeClass}">${
-                status ?? "no type"
-            }</span>
-        `
-                : ""
-        }
-    `;
-};
+import getTestArgs from "./label.test.setup";
 
 describe("label", () => {
-    runA11yTests({
-        baseClass: `s-label`,
-        modifiers: {
-            primary: ["sm", "md", "lg", "xl"],
-        },
-        // TODO: add abbr s-required-symbol variant
-        children: {
-            "default": getChildren(),
-            "status": getChildren("base"),
-            "status-beta": getChildren("beta"),
-            "status-new": getChildren("new"),
-            "status-required": getChildren("required"),
-        },
-        tag: "label",
-        template: ({ component, testid }) =>
-            labelTemplate({ component, testid }),
-    });
+    runA11yTests(getTestArgs());
 });
