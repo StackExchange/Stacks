24d6c0c539c49f06ee0580dc7379f36172ac7f4d
Dan Cormier <dancormierall@gmail.com>
2024-07-30 10:42:26 -0400

    Add visual test for s-link within paragraph

--- lib/components/link/link.visual.test.ts
+++ lib/components/link/link.visual.test.ts
@@ -28,4 +28,30 @@ describe("link", () => {
             <div data-testid="${testid}" class="p4 ws1">${component}</div>
         `,
     });
+
+    // Visual tests for link nested within a paragraph
+    runVisualTests({
+        baseClass: "s-link",
+        modifiers: {
+            primary: [
+                "grayscale",
+                "muted",
+                "danger",
+                "inherit",
+                "underlined",
+                "visited",
+            ],
+            secondary: ["dropdown"],
+        },
+        children: {
+            p: "s-link",
+        },
+        tag: "a",
+        attributes: {
+            href: "#",
+        },
+        template: ({ component, testid }) => html`
+            <p data-testid="${testid}" class="p4 ws1">${component}</p>
+        `,
+    });
 });
