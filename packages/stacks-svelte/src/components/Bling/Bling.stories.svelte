35fe29b0af58cafe4858e11f1c4061409d1c120b
Dan Cormier <dcormier@stackoverflow.com>
2025-11-17 11:14:10 -0500

    chore(bling): update to SHINE styling (#2013)
    * chore(bling): update to SHINE styling
    
    * Let's stick with "award bling" for now
    
    * Why was I doing `--`?
    
    * update tests
    
    * Update other baseline images
    
    * Update svelte component
    
    * update tests, formatting
    
    * Fix some tests
    
    * got a little lint in my pocket
    
    * Change name to bling
    
    * formatting!
    
    * Add TODO for migration guide
    
    * Address PR review
    
    * Remove award bling test images
    
    * update remaining visual regression test images
    
    * Update activity variant to use pink colors
    
    * changeset
    
    * Address review
    
    * Update docs, tests to use updated badge, migration guide
    
    * whoops
    
    * Add svelte link
    
    * Change rep bling color
    
    * Omit rep, activity base bling from svelte examples
    
    * Make the base bling appear as the base bling in svelte docs
    
    * Update docs
    
    * Minor tweaks to the docs

---
+++ packages/stacks-svelte/src/components/Bling/Bling.stories.svelte
@@ -0,0 +1,111 @@
+<script lang="ts" module>
+    import { defineMeta } from "@storybook/addon-svelte-csf";
+    import Bling, { type Type, type Size } from "./Bling.svelte";
+    const BlingSizes: Size[] = ["sm", "", "lg"];
+    const BlingTypes: Type[] = [
+        "",
+        "activity",
+        "rep",
+        "gold",
+        "silver",
+        "bronze",
+    ];
+
+    const { Story } = defineMeta({
+        title: "Components/Bling",
+        component: Bling,
+    });
+</script>
+
+<Story
+    name="Base"
+    args={{
+        name: "Bling",
+    }}
+>
+    {#snippet template(args)}
+        <Bling
+            type={args.type}
+            name={args.name}
+            size={args.size}
+            filled={args.filled}
+        />
+    {/snippet}
+</Story>
+
+<Story name="Types" asChild>
+    <div class="d-flex fd-column g64">
+        <table class="s-table s-table__bx-simple wmx7">
+            <thead>
+                <tr>
+                    <th scope="col">Type</th>
+                    <th scope="col" class="s-table--cell8">Example</th>
+                </tr>
+            </thead>
+            <tbody>
+                {#each BlingTypes as type (type)}
+                    {#if type !== "rep" && type !== "activity"}
+                        <tr>
+                            <th scope="row" class="va-middle">
+                                {type || "default"}
+                            </th>
+                            <td class="va-middle px8">
+                                <Bling name="{type} example" {type} />
+                            </td>
+                        </tr>
+                    {/if}
+                {/each}
+            </tbody>
+        </table>
+    </div>
+</Story>
+
+<Story name="Filled" asChild>
+    <div class="d-flex fd-column g64">
+        <table class="s-table s-table__bx-simple wmx7">
+            <thead>
+                <tr>
+                    <th scope="col">Type</th>
+                    <th scope="col" class="s-table--cell8">Example</th>
+                </tr>
+            </thead>
+            <tbody>
+                {#each BlingTypes as type (type)}
+                    <tr>
+                        <th scope="row" class="va-middle">
+                            {type || "default"}
+                        </th>
+                        <td class="va-middle px8">
+                            <Bling name="{type} example" {type} filled />
+                        </td>
+                    </tr>
+                {/each}
+            </tbody>
+        </table>
+    </div>
+</Story>
+
+<Story name="Sizes" asChild>
+    <div class="d-flex fd-column g64">
+        <table class="s-table s-table__bx-simple wmx7">
+            <thead>
+                <tr>
+                    <th scope="col">Type</th>
+                    <th scope="col" class="s-table--cell8">Example</th>
+                </tr>
+            </thead>
+            <tbody>
+                {#each BlingSizes as size (size)}
+                    <tr>
+                        <th scope="row" class="va-middle">
+                            {size || "default"}
+                        </th>
+                        <td class="va-middle px8">
+                            <Bling name="{size} example" {size} filled />
+                        </td>
+                    </tr>
+                {/each}
+            </tbody>
+        </table>
+    </div>
+</Story>
