0869c1fa0c69c96a4dc828612da0073d59ed71ff
Dan Cormier <dancormierall@gmail.com>
2025-12-12 16:57:17 -0500

    Fix .s-menu > .s-check-group gap; add visual test

--- packages/stacks-classic/lib/components/menu/menu.visual.test.ts
+++ packages/stacks-classic/lib/components/menu/menu.visual.test.ts
@@ -26,6 +26,27 @@ describe("menu", () => {
                     <a href="#" class="s-menu--action s-menu--action__danger">Danger link</a>
                 </li>
             `,
+            checkmark: `
+                <li class="s-menu--title" role="separator">Checkmark Menu</li>
+                <li class="s-menu--item" role="menuitem">
+                    <label class="s-menu--action s-check s-check__checkmark" for="check-1">
+                        <input type="checkbox" id="check-1" name="check-1" />
+                        Option 1
+                    </label>
+                </li>
+                <li class="s-menu--item" role="menuitem">
+                    <label class="s-menu--action s-check s-check__checkmark" for="check-2">
+                        <input type="checkbox" id="check-2" name="check-2" checked />
+                        Option 2
+                    </label>
+                </li>
+                <li class="s-menu--item" role="menuitem">
+                    <label class="s-menu--action s-check s-check__checkmark" for="check-3">
+                        <input type="checkbox" id="check-3" name="check-3" />
+                        Option 3
+                    </label>
+                </li>
+            `,
         },
         tag: "ul",
         attributes: {
