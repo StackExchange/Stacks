79e61a8f8cb482b6d11b15d7bd50133e2138f4a7
Sal <sbatirbek@stackoverflow.com>
2025-12-16 09:38:49 -0500

    update baseline

--- packages/stacks-classic/lib/components/empty-state/empty-state.visual.test.ts
+++ packages/stacks-classic/lib/components/empty-state/empty-state.visual.test.ts
@@ -1,5 +1,5 @@
 import { html } from "@open-wc/testing";
-import { SpotEmptyXL } from "@stackoverflow/stacks-icons-legacy";
+import { SpotEmpty } from "@stackoverflow/stacks-icons/spots";
 import { runVisualTests } from "../../test/visual-test-utils";
 import "../../index";
 
@@ -7,7 +7,7 @@ describe("empty-state", () => {
     runVisualTests({
         baseClass: "s-empty-state",
         children: {
-            default: `${SpotEmptyXL}<p class="mt24"><strong>Hello!</strong> This is a wonderful empty state component.</p>`,
+            default: `${SpotEmpty}<p>This is a wonderful empty state component.</p>`,
         },
         template: ({ component, testid }) => html`
             <div class="ws3 p16" data-testid="${testid}">${component}</div>
