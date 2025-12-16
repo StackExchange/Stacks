7912c01a1c4733c76f7b5f15d2679a1e608dd160
David Longworth <djl@sent.com>
2025-12-16 19:49:03 +0000

    fix thumb height
    Signed-off-by: David Longworth <djl@sent.com>

--- packages/stacks-docs/src/routes/[category]/+page.svelte
+++ packages/stacks-docs/src/routes/[category]/+page.svelte
@@ -8,7 +8,7 @@
 
   {#each data?.active?.items as subsection}
     <a class="ff-stack-sans-headline d-flex bb bc-black-200 fs-title ai-center py16" href={subsection.externalUrl || `/${data?.active?.slug}/${subsection.slug}/`}>
-      <img width="255" height="134" class="w20 mr16 bg-black-150" src={subsection.image} alt="Thumbnail" />
+      <img width="255" height="134" class="w20 h-auto mr16 bg-black-150" src={subsection.image} alt="Thumbnail" />
       {subsection?.title}
     </a>
   {/each}
