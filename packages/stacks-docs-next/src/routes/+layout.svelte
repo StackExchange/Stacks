699238787cc3e1e3eae5080612e2994e85eb4d81
David Longworth <djl@sent.com>
2025-12-21 02:35:34 +0000

    banner text
    Signed-off-by: David Longworth <djl@sent.com>

--- packages/stacks-docs-next/src/routes/+layout.svelte
+++ packages/stacks-docs-next/src/routes/+layout.svelte
@@ -56,7 +56,7 @@
 
 <main class="main bg-white hmn-screen d-flex fd-column t24 sm:t0 ps-relative">
 	<div class="bg-blue-400 fc-white px24 py12">
-		This is a preview focused on branding – for developer reference please see the <a href="https://stackoverflow.design" class="s-link fc-white s-link__underlined">curent version</a> or <a href="https://beta.stackoverflow.design" class="s-link fc-white s-link__underlined">beta version</a>.
+		This is a brand focused preview – for developer reference please see <a href="https://stackoverflow.design" class="s-link fc-white s-link__underlined">the curent docs</a> or <a href="https://beta.stackoverflow.design" class="s-link fc-white s-link__underlined">the beta release</a>.
 	</div>
 
 	<div class="my-auto">
