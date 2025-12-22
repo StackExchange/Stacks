88ff2759f5697a5570b596555df020e44f3f4a5f
David Longworth <djl@sent.com>
2025-12-22 13:54:42 +0000

    try no baseURL
    Signed-off-by: David Longworth <djl@sent.com>

--- packages/stacks-docs-next/src/lib/auth-client.ts
+++ packages/stacks-docs-next/src/lib/auth-client.ts
@@ -1,8 +1,6 @@
 import { createAuthClient } from "better-auth/client";
 import { genericOAuthClient } from "better-auth/client/plugins";
-import { browser } from "$app/environment";
 
 export const authClient = createAuthClient({
-    baseURL: browser ? window.location.origin : "http://localhost:5173",
     plugins: [genericOAuthClient()],
 });
