d53cdd189e6b831ea2db442068b26ab7c281f3b8
Tavian Taylor <ttaylor@stackoverflow.com>
2025-12-22 19:06:46 +0000

    feat(input) - update radio and checkbox components for shine (#2060)
    * Update docs - TEMP change
    
    * add checkmark class
    
    * update docs
    
    * Add radio svelte component
    
    * Add Checkbox and Checkmark svelte components
    
    * add changeset
    
    * fix tests
    
    * remove todo
    
    * use black-300 instead of opacity for checkmark
    
    * update a11y and visual tests
    
    * Update site-navigation.json
    
    * update tests
    
    * update based on comments
    
    * update visual regression tests
    
    * visual tests
    
    * Checkbox, radio refactor (#2078)
    
    * Checkbox, radio refactor
    
    * Cleanup
    
    * update based on comments
    
    * update based on comments
    
    * update visual tests
    
    * Update based on comments
    
    * Update Checkmark.test.ts
    
    * Docs updates for checkbox, radio, checkmark, menu (#2090)
    
    * Update menu docs
    
    * Update docs, minor styling fixes
    
    * Update check control tests
    
    * cleanup
    
    * Replace Checkbox/Radio w/ CheckControl, Add CheckGroup
    
    * Modify docs icons class appending to support new icons
    
    * Minor updates
    
    * Create MenuCheckItem
    
    * Make type prop optional
    
    * Add CheckControl, CheckGroup tests
    
    * Update Menu tests
    
    * visual test images
    
    * formatting
    
    * fix lint errors
    
    * revert table test image changes not related to this PR
    
    * revert select test image changes not related to this PR
    
    * Remove .s-check, .s-radio, rename .s-check-control to .s-check
    
    * remove a couple missed .s-checkbox, .s-radio references
    
    * Update table test images that contain checkboxes
    
    * Rename docs page to "Check"
    
    * Fix minor bugs; include disabled input in visual test
    
    * Fix .s-menu > .s-check-group gap; add visual test
    
    * Replace .s-check with .s-checkbox, .s-radio
    
    * Fix validation state border color
    
    * Add Checkbox, Radio Svelte components; hide Check
    
    * Give checkbox and radio independent docs pages
    
    * Split .s-check-group into .s-checkbox-group, .s-radio-group
    
    * Split CheckGroup into CheckboxGroup, RadioGroup
    
    * Rename group files in stacks-classic
    
    * Regenerate visual regresssion test images
    
    * Formatting
    
    * Revert some screenshots to reduce PR noise
    
    * Replace CheckboxGroup, RadioGroup with FormGroup
    
    * Remove Check
    
    * Update Radio, Checkbox
    
    * CheckboxGroup, RadioGroup updates
    
    * Minor docs updates
    
    * target correct classes in menu
    
    * Update table tests
    
    * Consolidate Svelte structure, add/update tests
    
    * Formatting and linting
    
    * Update stacks svelte radio link
    
    * Add stories for change events and bindings
    
    * Lift `onValueChange` up to `FormGroup`
    
    * Update CheckboxGroup story with onValueChange
    
    * Update migration guide, changeset
    
    * Minor fixes
    
    * Minor fixes
    
    * update visual tests
    
    ---------
    
    Co-authored-by: Dan Cormier <dcormier@stackoverflow.com>
    Co-authored-by: Dan Cormier <dancormierall@gmail.com>

--- packages/stacks-svelte/src/components/Menu/Menu.test.ts
+++ packages/stacks-svelte/src/components/Menu/Menu.test.ts
@@ -9,6 +9,7 @@ import type { Component } from "svelte";
 
 import Menu from "./Menu.svelte";
 import MenuItem from "./MenuItem.svelte";
+import MenuCheckItem from "./MenuCheckItem.svelte";
 import MenuTitle from "./MenuTitle.svelte";
 import MenuDivider from "./MenuDivider.svelte";
 import { IconSettings } from "@stackoverflow/stacks-icons/icons";
@@ -32,6 +33,12 @@ describe("Menu", () => {
             props,
         }) as ComponentMeta<Component>;
 
+    const createMenuCheckItem = (props: Record<string, unknown>) =>
+        ({
+            component: MenuCheckItem,
+            props,
+        }) as ComponentMeta<Component>;
+
     const children = (text?: string) =>
         createRawSnippet(() => ({
             render: () => `<span>${text ?? ""}</span>`,
@@ -46,10 +53,23 @@ describe("Menu", () => {
 
         render(Menu, { children: menuItems });
 
-        expect(screen.getByRole("menu")).to.exist;
+        const menu = screen.getByRole("menu");
+        expect(menu).to.exist;
+        expect(menu.tagName).to.equal("UL");
+        expect(menu).to.have.attribute("role", "menu");
         expect(screen.getByText("Share")).to.exist;
         expect(screen.getByText("Edit")).to.exist;
         expect(screen.getAllByRole("menuitem")).to.have.length(2);
+    });
+
+    it("should render as a fieldset when type is fieldset", () => {
+        render(Menu, { type: "fieldset", children: children() });
+
+        const fieldset = document.querySelector("fieldset.s-menu");
+        expect(fieldset).to.exist;
+        expect(fieldset).to.have.class("s-menu");
+        expect(fieldset!.tagName).to.equal("FIELDSET");
+        expect(fieldset).not.to.have.attribute("role");
     });
 
     it("should render with arbitrary classes", () => {
@@ -77,7 +97,9 @@ describe("Menu", () => {
 
     it("should render menu item as button when href is not provided", () => {
         const menuItems = createSvelteComponentsSnippet([
-            createMenuItem({ children: children("Delete") }),
+            createMenuItem({
+                children: children("Delete"),
+            }),
         ]);
 
         render(Menu, { children: menuItems });
@@ -103,51 +125,193 @@ describe("Menu", () => {
         expect(link).to.have.class("s-menu--action__danger");
     });
 
-    it("should render menu item with selected styling when selected prop is true", () => {
-        const menuItems = createSvelteComponentsSnippet([
-            createMenuItem({
-                href: "#",
-                selected: true,
-                children: children("Current Item"),
+    // Menu Check Item
+    it("should render menu check item with required props", () => {
+        const menuCheckItems = createSvelteComponentsSnippet([
+            createMenuCheckItem({
+                id: "test-checkbox-1",
+                name: "test-name",
+                label: "Option 1",
+                type: "checkbox",
             }),
         ]);
 
-        render(Menu, { children: menuItems });
+        render(Menu, { type: "fieldset", children: menuCheckItems });
 
-        const link = screen.getByRole("menuitem").querySelector("a");
-        expect(link).to.have.class("is-selected");
+        expect(screen.getByText("Option 1")).to.exist;
+        expect(screen.getByRole("checkbox")).to.exist;
+        const container = screen.getByText("Option 1").closest(".s-menu--item");
+        expect(container).to.exist;
     });
 
-    it("should render menu item with custom i18nSelectedLabel when selected", () => {
-        const menuItems = createSvelteComponentsSnippet([
-            createMenuItem({
-                href: "#",
-                selected: true,
-                i18nSelectedLabel: "Sélectionné",
-                children: children("Current Item"),
+    it("should render menu check item as checkbox type", () => {
+        const menuCheckItems = createSvelteComponentsSnippet([
+            createMenuCheckItem({
+                id: "test-checkbox-1",
+                name: "test-name",
+                label: "Option 1",
+                type: "checkbox",
             }),
         ]);
 
-        render(Menu, { children: menuItems });
+        render(Menu, { type: "fieldset", children: menuCheckItems });
 
-        expect(screen.getByText("Sélectionné")).to.exist;
-        const selectedLabel = screen.getByText("Sélectionné");
-        expect(selectedLabel).to.have.class("v-visible-sr");
+        const input = screen.getByRole("checkbox");
+        expect(input).to.have.attribute("type", "checkbox");
+        expect(input).to.have.id("test-checkbox-1");
+        expect(input).to.have.attribute("name", "test-name");
     });
 
-    it("should render menu item with icon when icon prop is provided", () => {
-        const menuItems = createSvelteComponentsSnippet([
-            createMenuItem({
-                href: "#",
+    it("should render menu check item as radio type", () => {
+        const menuCheckItems = createSvelteComponentsSnippet([
+            createMenuCheckItem({
+                id: "test-radio-1",
+                name: "test-radio-group",
+                label: "Option 1",
+                type: "radio",
+            }),
+        ]);
+
+        render(Menu, { type: "fieldset", children: menuCheckItems });
+
+        const input = screen.getByRole("radio");
+        expect(input).to.have.attribute("type", "radio");
+        expect(input).to.have.id("test-radio-1");
+        expect(input).to.have.attribute("name", "test-radio-group");
+    });
+
+    it("should render menu check item with checked state when checked is true", () => {
+        const menuCheckItems = createSvelteComponentsSnippet([
+            createMenuCheckItem({
+                id: "test-checkbox-1",
+                name: "test-name",
+                label: "Option 1",
+                type: "checkbox",
+                checked: true,
+            }),
+        ]);
+
+        render(Menu, { type: "fieldset", children: menuCheckItems });
+
+        const input = screen.getByRole("checkbox");
+        expect(input).to.have.property("checked", true);
+    });
+
+    it("should render menu check item with description when provided", () => {
+        const menuCheckItems = createSvelteComponentsSnippet([
+            createMenuCheckItem({
+                id: "test-checkbox-1",
+                name: "test-name",
+                label: "Option 1",
+                type: "checkbox",
+                description: "Test description",
+            }),
+        ]);
+
+        render(Menu, { type: "fieldset", children: menuCheckItems });
+
+        const description = screen.getByText("Test description");
+        expect(description).to.exist;
+        expect(description).to.have.class("s-description");
+    });
+
+    it("should render menu check item disabled when disabled is true", () => {
+        const menuCheckItems = createSvelteComponentsSnippet([
+            createMenuCheckItem({
+                id: "test-checkbox-1",
+                name: "test-name",
+                label: "Option 1",
+                type: "checkbox",
+                disabled: true,
+            }),
+        ]);
+
+        render(Menu, { type: "fieldset", children: menuCheckItems });
+
+        const input = screen.getByRole("checkbox");
+        expect(input).to.have.attribute("disabled");
+    });
+
+    it("should render menu check item with icon when icon prop is provided", () => {
+        const menuCheckItems = createSvelteComponentsSnippet([
+            createMenuCheckItem({
+                id: "test-checkbox-1",
+                name: "test-name",
+                label: "Option 1",
+                type: "checkbox",
                 icon: IconSettings,
-                children: children("Share"),
             }),
         ]);
 
-        render(Menu, { children: menuItems });
+        render(Menu, { type: "fieldset", children: menuCheckItems });
 
         const icon = document.querySelector(".s-menu--icon");
         expect(icon).to.exist;
+    });
+
+    it("should render menu check item with value attribute", () => {
+        const menuCheckItems = createSvelteComponentsSnippet([
+            createMenuCheckItem({
+                id: "test-checkbox-1",
+                name: "test-name",
+                label: "Option 1",
+                type: "checkbox",
+                value: 42,
+            }),
+        ]);
+
+        render(Menu, { type: "fieldset", children: menuCheckItems });
+
+        const input = screen.getByRole("checkbox");
+        expect(input).to.have.attribute("value", "42");
+    });
+
+    it("should render menu check item with arbitrary classes", () => {
+        const menuCheckItems = createSvelteComponentsSnippet([
+            createMenuCheckItem({
+                id: "test-checkbox-1",
+                name: "test-name",
+                label: "Option 1",
+                type: "checkbox",
+                class: "custom-class",
+            }),
+        ]);
+
+        render(Menu, { type: "fieldset", children: menuCheckItems });
+
+        const container = screen.getByText("Option 1").closest(".s-menu--item");
+        expect(container).to.have.class("custom-class");
+        expect(container).to.have.class("s-menu--item");
+    });
+
+    it("should render multiple menu check items", () => {
+        const menuCheckItems = createSvelteComponentsSnippet([
+            createMenuCheckItem({
+                id: "test-checkbox-1",
+                name: "test-group",
+                label: "Option 1",
+                type: "checkbox",
+            }),
+            createMenuCheckItem({
+                id: "test-checkbox-2",
+                name: "test-group",
+                label: "Option 2",
+                type: "checkbox",
+            }),
+            createMenuCheckItem({
+                id: "test-checkbox-3",
+                name: "test-group",
+                label: "Option 3",
+                type: "checkbox",
+            }),
+        ]);
+
+        render(Menu, { type: "fieldset", children: menuCheckItems });
+
+        expect(screen.getAllByRole("checkbox")).to.have.length(3);
+        expect(screen.getByText("Option 1")).to.exist;
+        expect(screen.getByText("Option 2")).to.exist;
+        expect(screen.getByText("Option 3")).to.exist;
     });
 
     it("should render menu item with arbitrary classes", () => {
@@ -176,6 +340,24 @@ describe("Menu", () => {
         const title = screen.getByRole("separator");
         expect(title).to.exist;
         expect(title).to.have.class("s-menu--title");
+        expect(screen.getByText("Section Title")).to.exist;
+    });
+
+    it("should render menu title as legend when type is legend", () => {
+        const menuWithTitle = createSvelteComponentsSnippet([
+            createMenuTitle({
+                type: "legend",
+                children: children("Section Title"),
+            }),
+        ]);
+
+        render(Menu, { type: "fieldset", children: menuWithTitle });
+
+        const legend = document.querySelector("legend.s-menu--title");
+        expect(legend).to.exist;
+        expect(legend).to.have.class("s-menu--title");
+        expect(legend!.tagName).to.equal("LEGEND");
+        expect(legend).not.to.have.attribute("role");
         expect(screen.getByText("Section Title")).to.exist;
     });
 
