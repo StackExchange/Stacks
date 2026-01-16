7fc4fefbe935adeef5f22e119f397e1c178bd8d5
Dan Cormier <dancormierall@gmail.com>
2026-01-15 21:13:27 -0500

    Eating some dogfood

--- packages/stacks-docs/plugins/header.js
+++ packages/stacks-docs/plugins/header.js
@@ -8,9 +8,20 @@ module.exports = {
       var slug = text.replace(/\s+/g, '-').toLowerCase();
       var linkIcon = Icons.IconLink;
 
+      var baseHeadingClasses = "fl-grow1 mb0 lh-sm";
+      var headingClasses = "fs-display1 mb8";
+      if (tag === "h2") {
+        headingClasses = " fs-headline1";
+      } else if (tag === "h3") {
+        headingClasses = " fs-subheading fc-black-500";
+      } else if (tag === "h4") {
+        headingClasses = " fs-body3 fc-black-400";
+      }
+      var headingTag = '<' + tag + ' class="' + baseHeadingClasses + ' ' + headingClasses + '" id="'+ slug +'">';
+
       var output = '';
       output += '<div class="d-flex jc-space-between ai-end mb12">';
-      output +=   '<' + tag + ' class="fl-grow1 stacks-' + tag + '" id="'+ slug +'">';
+      output +=   headingTag;
       output +=     text;
       output +=   '</' + tag + '>';
       output +=   '<a class="s-btn s-btn__clear" href="#'+ slug +'">';
