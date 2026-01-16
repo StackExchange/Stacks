c8e299c4e857eb50ad4edb2706302cd1d8977265
Tavian Taylor <ttaylor@stackoverflow.com>
2026-01-16 09:25:59 +0000

    Svelte component

--- packages/stacks-svelte/src/components/UserCard/UserCardBadge.svelte
+++ packages/stacks-svelte/src/components/UserCard/UserCardBadge.svelte
@@ -1,14 +1,34 @@
 <script lang="ts">
+    import type { Snippet } from "svelte";
     import Badge, { type UserType } from "../Badge/Badge.svelte";
+    import Popover from "../Popover/Popover.svelte";
+    import PopoverContent from "../Popover/PopoverContent.svelte";
+    import PopoverReference from "../Popover/PopoverReference.svelte";
     interface Props {
         /**
          * The type of user badge
          */
         type: UserType;
+
+        /**
+         * Snippet used to display tooltip content
+         */
+        tooltip?: Snippet;
     }
-    const { type }: Props = $props();
+    const { type, tooltip }: Props = $props();
 </script>
 
 <li>
-    <Badge text={type} type="user" userType={type} size="sm" />
+    {#if tooltip && type === "new"}
+        <Popover id="user-card-badge-new-popover" tooltip>
+            <PopoverReference>
+                <Badge text={type} type="user" userType={type} size="sm" />
+            </PopoverReference>
+            <PopoverContent>
+                {@render tooltip()}
+            </PopoverContent>
+        </Popover>
+    {:else}
+        <Badge text={type} type="user" userType={type} size="sm" />
+    {/if}
 </li>
