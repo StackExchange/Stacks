12083d70077c09bdd93a3ed7f310db51efd5f786
Dan Cormier <dancormierall@gmail.com>
2026-02-02 20:08:55 -0500

    Split misc.less and others into discrete groupings

--- packages/stacks-classic/lib/atomic/box-shadow.less.test.ts
+++ packages/stacks-classic/lib/atomic/box-shadow.less.test.ts
@@ -1,8 +1,8 @@
 import { describe, it, expect } from "vitest";
 import { renderLess } from "../test/less-test-utils";
 
-describe("atomic: misc", () => {
-    it("should output all atomic css classes", async () => {
+describe("atomic: box-shadow", () => {
+    it("should output all atomic box-shadow classes", async () => {
         const css = await renderLess(`
             @import "./lib/atomic/box-shadow.less";
         `);
--- packages/stacks-classic/lib/atomic/current-color.less.test.ts
+++ packages/stacks-classic/lib/atomic/current-color.less.test.ts
@@ -1,10 +1,10 @@
 import { describe, it, expect } from "vitest";
 import { renderLess } from "../test/less-test-utils";
 
-describe("atomic: misc", () => {
-    it("should output all atomic css classes", async () => {
+describe("atomic: current-color", () => {
+    it("should output all atomic current-color classes", async () => {
         const css = await renderLess(`
-            @import "./lib/atomic/misc.less";
+            @import "./lib/atomic/current-color.less";
         `);
 
         expect(css).toMatchSnapshot();
