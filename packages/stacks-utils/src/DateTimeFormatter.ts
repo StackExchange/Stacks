98f9b903fa57494c0c01e0f41e52fc644115fc77
Giamir Buoncristiani <giamir.buoncristiani@gmail.com>
2026-02-06 11:00:39 +0100

    build(stacks-utils): ensure correct module resolution in nodejs

--- packages/stacks-utils/src/DateTimeFormatter.ts
+++ packages/stacks-utils/src/DateTimeFormatter.ts
@@ -1,6 +1,6 @@
 ﻿import dayjs from "dayjs";
-import relativeTime from "dayjs/plugin/relativeTime";
-import updateLocale from "dayjs/plugin/updateLocale";
+import relativeTime from "dayjs/plugin/relativeTime.js";
+import updateLocale from "dayjs/plugin/updateLocale.js";
 
 dayjs.extend(relativeTime);
 dayjs.extend(updateLocale);
