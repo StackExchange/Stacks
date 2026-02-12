9780a0eeef0a2a08eb935b239ae628e4e4af3f51
Sal <sbatirbek@stackoverflow.com>
2026-02-12 17:34:26 -0500

    svelte changes

--- packages/stacks-svelte/src/components/Navigation/NavigationGroup.svelte
+++ packages/stacks-svelte/src/components/Navigation/NavigationGroup.svelte
@@ -1,32 +1,52 @@
 <script lang="ts">
     import type { Snippet } from "svelte";
+    import type { ClassValue } from "svelte/elements";
+
     interface Props {
         /**
          * The title of the navigation section
          */
         title: string;
         /**
-         * Additional CSS classes added to the element
+         * Html tag to use for the title: h1, h2, h3, h4, etc.
          */
-        class?: string;
+        titleTag?: string;
+        /**
+         * Additional CSS classes added to the outer element
+         */
+        class?: ClassValue;
+        /**
+         * Additional CSS classes added to the title element
+         */
+        titleClass?: ClassValue;
         /**
          * Optional content rendered at the end of the navigation title.
          */
         trailing?: Snippet;
+        /**
+         * The navigation items rendered inside the group
+         */
+        children: Snippet;
     }
-    let { title, class: className = "", trailing }: Props = $props();
-    const getClasses = (className: string) => {
-        const base = "s-navigation--title";
-        let classes = "d-flex jc-space-between " + base;
-        if (className) {
-            classes += ` ${className}`;
-        }
-        return classes;
-    };
-    const classes = $derived(getClasses(className));
+
+    let {
+        title,
+        titleTag = "h4",
+        class: className = "",
+        titleClass = "",
+        trailing,
+        children,
+    }: Props = $props();
 </script>
 
-<li class={classes} role="separator">
-    {title}
-    {@render trailing?.()}
+<li class={className}>
+    <div class={["s-navigation--title", "d-flex", titleClass]}>
+        <svelte:element this={titleTag} class="mb0 fw-normal">
+            {title}
+        </svelte:element>
+        {@render trailing?.()}
+    </div>
+    <ul>
+        {@render children?.()}
+    </ul>
 </li>
