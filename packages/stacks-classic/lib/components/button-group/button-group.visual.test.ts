2ccdfaf1a83469347a6ad678445fa1709e28e363
Dan Cormier <dcormier@stackoverflow.com>
2026-02-26 14:16:16 -0500

    chore: remove cruft left over from removing components (#2191)
    * chore: remove cruft left over from removing components
    
    * remove s-card
    
    * remove topbar
    
    * remove visual regression test images no longer needed
    
    * Update anchor test images
    
    * changeset
    
    * One more file can go
    
    * Remove some topbar cruft
    
    * Fix header icon colors
    
    * Remove TOC on nav for removed component docs pages

--- packages/stacks-classic/lib/components/button-group/button-group.visual.test.ts
+++
@@ -1,7 +0,0 @@
-import { testArgs } from "./button-group.test.setup";
-import { runVisualTests } from "../../test/visual-test-utils";
-import "../../index";
-
-describe.skip("button group", () => {
-    runVisualTests(testArgs);
-});
