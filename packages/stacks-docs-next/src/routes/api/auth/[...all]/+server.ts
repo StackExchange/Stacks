47136f9bffb1ae9b26bd8d9b003576af0a50cb19
David Longworth <djl@sent.com>
2026-03-20 11:49:35 +0000

    tidy unused

--- packages/stacks-docs-next/src/routes/api/auth/[...all]/+server.ts
+++ packages/stacks-docs-next/src/routes/api/auth/[...all]/+server.ts
@@ -1,6 +1,5 @@
 import { auth } from "$lib/auth";
 import { toSvelteKitHandler } from "better-auth/svelte-kit";
-import type { RequestEvent } from "@sveltejs/kit";
 
 const handler = toSvelteKitHandler(auth);
 
