47a3c3774746c0fe71097212dd048e26ffe60d8d
Dan Cormier <dancormierall@gmail.com>
2026-04-07 13:14:44 -0400

    quiet perf warnings

--- packages/stacks-svelte/test-utils.d.ts
+++
@@ -1,8 +0,0 @@
-import { type Component, type ComponentProps, type SvelteComponent } from "svelte";
-declare const createRawComponent: (code: string) => Promise<typeof SvelteComponent>;
-interface ComponentMeta<T extends Component> {
-    component: T;
-    props: ComponentProps<T>;
-}
-declare const createSvelteComponentsSnippet: (components: ComponentMeta<any>[]) => import("svelte").Snippet<[]>;
-export { createRawComponent, createSvelteComponentsSnippet, type ComponentMeta, };
