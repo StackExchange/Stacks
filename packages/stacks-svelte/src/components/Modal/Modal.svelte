7e44969454bb630f543c414cfaa5b639a5d8dc94
Andrea Sánchez Blanco <asanchezblanco@stackoverflow.com>
2026-07-08 13:45:58 +0200

    Cleanup unneeded preventDefault

--- packages/stacks-svelte/src/components/Modal/Modal.svelte
+++ packages/stacks-svelte/src/components/Modal/Modal.svelte
@@ -88,7 +88,6 @@
 
     const keyPress = (event: KeyboardEvent) => {
         if (event.key === "Escape" && visible) {
-            event.preventDefault();
             event.stopImmediatePropagation();
 
             close();
