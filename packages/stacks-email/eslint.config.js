da44118fbd83251f858b0f00f7600e5b83e6e57c
Dan Cormier <dancormierall@gmail.com>
2026-07-22 15:06:54 -0400

    feat(email): publish package artifacts

--- packages/stacks-email/eslint.config.js
+++ packages/stacks-email/eslint.config.js
@@ -6,7 +6,7 @@ import prettier from "eslint-config-prettier";
 
 export default tseslint.config(
     {
-        ignores: [".svelte-kit/**"],
+        ignores: [".svelte-kit/**", "dist/**"],
     },
     js.configs.recommended,
     ...tseslint.configs.recommended,
