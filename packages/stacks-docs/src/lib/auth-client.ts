dfb27f8a0e9ce457e90db31368b7ded69707012a
Andrea Sánchez Blanco <asanchezblanco@stackoverflow.com>
2026-09-24 12:07:18 +0200

    fix(deps): support better-auth 1.7.5

--- packages/stacks-docs/src/lib/auth-client.ts
+++ packages/stacks-docs/src/lib/auth-client.ts
@@ -1,6 +1,3 @@
 import { createAuthClient } from "better-auth/client";
-import { genericOAuthClient } from "better-auth/client/plugins";
 
-export const authClient = createAuthClient({
-    plugins: [genericOAuthClient()],
-});
+export const authClient = createAuthClient();
